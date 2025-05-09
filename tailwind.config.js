171789bbdd3e071e4266acaf75384e09800d21ca
seongmin <min@seongminui-MacBookPro.local>
2025-05-09 15:08:30 +0900

    chore: 커스텀 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ const config = {
       fontFamily: {
         sans: ["Pretendard", "sans-serif"],
       },
+      colors: {
+        violet300: "#EADFFF",
+        violet600: "#68499F",
+
+        gray900: "#22242B",
+        gray600: "#73767E",
+        gray200: "#EBECEF",
+
+        skyblue100: "#EAEDF7",
+        skyblue200: "#EBEDF8",
+        skyblue300: "#D1D6E8",
+
+        red200: "#FFA49D",
+        green200: "#C9EDE9",
+      },
     },
   },
   plugins: [],
