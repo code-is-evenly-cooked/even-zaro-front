ec5852e773df642a5cb1f9996ea12338704ed762
seongmin <min@seongminui-MacBookPro.local>
2025-05-18 18:36:51 +0900

    feat: api 주소 상수로 추가

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,20 +1,19 @@
 import { NextRequest } from "next/server";
 import { createSuccessResponse, createErrorResponse } from "@/lib/response";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/signup`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
+    const res = await fetch(`${API_BASE_URL}/auth/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify(body),
+    });
 
     const data = await res.json();
     if (!res.ok) {
