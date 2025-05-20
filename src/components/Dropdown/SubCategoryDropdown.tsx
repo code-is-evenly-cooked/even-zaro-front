8e41bbb1a168e6b0501a03592098952fa38e9b8c
seongmin <min@seongminui-MacBookPro.local>
2025-05-20 11:54:51 +0900

    feat: 서브 카테고리 추가

---
+++ src/components/Dropdown/SubCategoryDropdown.tsx
@@ -0,0 +1,72 @@
+import { CATEGORY_MAP } from "@/constants/category";
+import { getSubCategoryLabel } from "@/utils/category";
+import clsx from "clsx";
+import { ChevronDown } from "lucide-react";
+import type { MainCategory, SubCategoryValue } from "@/types/category";
+
+interface SubCategoryDropdownProps {
+  selectedMainCategory: MainCategory;
+  selectedSubCategory: SubCategoryValue | "전체";
+  isDropdownOpen: boolean;
+  toggleDropdown: () => void;
+  selectSubCategory: (subCategory: SubCategoryValue | "전체") => void;
+  buttonRef: React.RefObject<HTMLButtonElement | null>;
+  buttonWidth: number;
+}
+
+const SubCategoryDropdown = ({
+  selectedMainCategory,
+  selectedSubCategory,
+  isDropdownOpen,
+  toggleDropdown,
+  selectSubCategory,
+  buttonRef,
+  buttonWidth,
+}: SubCategoryDropdownProps) => {
+  const category =
+    CATEGORY_MAP[selectedMainCategory as keyof typeof CATEGORY_MAP];
+
+  // 잘못된 mainCategory가 들어올 경우 렌더링 안 함
+  if (!category) return null;
+
+  const subCategories = category.options;
+
+  return (
+    <div className="relative">
+      <button
+        ref={buttonRef}
+        type="button"
+        className="flex items-center whitespace-nowrap bg-skyblue300 text-gray600 py-2 pl-3 pr-2 rounded-lg"
+        onClick={toggleDropdown}
+      >
+        {selectedSubCategory === "전체"
+          ? "태그 선택"
+          : getSubCategoryLabel(selectedSubCategory)}
+        <ChevronDown className="w-4 h-4 ml-2" />
+      </button>
+
+      {isDropdownOpen && (
+        <ul
+          className="absolute z-10 top-full mt-2 w-max bg-skyblue300 rounded-lg p-2 shadow-md text-gray900"
+          style={{ minWidth: buttonWidth }}
+        >
+          {subCategories.map((option) => (
+            <li
+              key={option.tag}
+              onClick={() => selectSubCategory(option.tag)}
+              className={clsx(
+                "px-2 py-1 text-gray600 hover:text-gray600/80 cursor-pointer",
+                selectedSubCategory === option.tag &&
+                  "font-bold text-gray900 hover:text-gray900",
+              )}
+            >
+              {option.emoji} {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SubCategoryDropdown;
