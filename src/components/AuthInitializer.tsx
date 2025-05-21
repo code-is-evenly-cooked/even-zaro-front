549c82afe8b59997df9931c0b311f6be7a83b441
seongmin <min@seongminui-MacBookPro.local>
2025-05-21 19:48:28 +0900

    feat: AuthInitializer에 reactquery 적용

--- src/components/AuthInitializer.tsx
+++ src/components/AuthInitializer.tsx
@@ -2,41 +2,45 @@ import { useEffect } from "react";
 import { getSession } from "next-auth/react";
 import { useAuthStore, UserInfo } from "@/stores/useAuthStore";
 import { client } from "@/lib/fetch/client";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchUserKakaoAccessToken = async (): Promise<UserInfo> => {
+  const session = await getSession();
+  const kakaoAccessToken = session?.user?.accessToken;
+
+  if (kakaoAccessToken) {
+    return await client<UserInfo>("/api/auth/signin/social", {
+      method: "POST",
+      body: JSON.stringify({
+        accessToken: kakaoAccessToken,
+      }),
+    });
+  } else {
+    return await client<UserInfo>("/api/users/my");
+  }
+};
 
 const AuthInitializer = ({ children }: { children: React.ReactNode }) => {
   const { setUser, clearUser, setInitialized, isInitialized } = useAuthStore();
 
-  const initializeAuth = async () => {
-    try {
-      const session = await getSession();
-      const kakaoAccessToken = session?.user?.accessToken;
-      let user: UserInfo | null = null;
-
-      if (kakaoAccessToken) {
-        user = await client<UserInfo>("/api/auth/signin/social", {
-          method: "POST",
-          body: JSON.stringify({
-            accessToken: kakaoAccessToken,
-          }),
-        });
-      } else {
-        user = await client<UserInfo>("/api/users/my");
-      }
-
-      setUser(user);
-    } catch (err) {
-      console.error("AuthInitializer Error:", err);
-      clearUser();
-    } finally {
-      setInitialized();
-    }
-  };
+  const { data, error, isSuccess } = useQuery({
+    queryKey: ["user"],
+    queryFn: fetchUserKakaoAccessToken,
+    enabled: !isInitialized,
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
+  });
 
   useEffect(() => {
-    if (!isInitialized) {
-      initializeAuth();
+    if (!isSuccess && data) {
+      setUser(data);
+      setInitialized();
     }
-  }, [isInitialized]);
+    if (error) {
+      clearUser();
+      setInitialized();
+    }
+  }, [isSuccess, error, data]);
 
   return <>{children}</>;
 };
