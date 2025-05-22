ecbe88df02d4067316ab6c2c156eb7c60d7ed22b
SangHwi <brilyent72@gmail.com>
2025-05-22 20:14:28 +0900

    feat: 게시글 상세 상단 컴포넌트 구현 및 추가 (카테고리, 태그, 제목, 작성 일 )

--- src/app/board/[category]/[postId]/page.tsx
+++ src/app/board/[category]/[postId]/page.tsx
@@ -1,12 +1,21 @@
 import { fetchPostDetail } from "@/lib/api/post";
+import PostHeader from "@/components/post/PostHeader";
 
-export default async function PostDetailPage({ params }: { params: { postId: string } }) {
-    const post = await fetchPostDetail(params.postId);
+export default async function PostDetailPage({
+  params,
+}: {
+  params: { postId: string };
+}) {
+  const post = await fetchPostDetail(params.postId);
 
   return (
     <main className="px-6 py-10">
-      <h1 className="text-2xl font-bold">{post.data.title}</h1>
-      <p className="text-sm text-gray-500">{post.data.user.nickname}</p>
+      <PostHeader
+        category={post.data.category}
+        tag={post.data.tag}
+        title={post.data.title}
+        createdAt={post.data.createdAt}
+      />
     </main>
   );
 }
