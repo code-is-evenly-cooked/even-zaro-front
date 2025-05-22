27ef2ffde7732a76888ac01f59ce30dce4d02599
SangHwi <brilyent72@gmail.com>
2025-05-22 22:47:14 +0900

    feat: 게시글 상세에서 작성자 프로필이 null 일 경우 기본 이미지 추가

--- src/components/post/PostAuthor.tsx
+++ src/components/post/PostAuthor.tsx
@@ -1,17 +1,19 @@
 interface PostAuthorProps {
   nickname: string;
-  profileImageUrl: string;
+  profileImageUrl: string | null;
 }
 
 export default function PostAuthor({
   nickname,
   profileImageUrl,
 }: PostAuthorProps) {
+  const defaultImage = "/icons/defaultProfile.svg";
+  const imageUrl = profileImageUrl || defaultImage;
   return (
     <div className="flex items-center justify-between my-3 py-3 border-b border-gray-600">
       <div className="flex items-center gap-3">
         <img
-          src={profileImageUrl}
+          src={imageUrl}
           alt="프로필"
           className="w-10 h-10 rounded-full object-cover"
         />
