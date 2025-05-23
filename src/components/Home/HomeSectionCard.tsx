d25210f0283ab35637369cd38e7465c4a38273ac
seongmin <min@seongminui-MacBookPro.local>
2025-05-23 15:01:18 +0900

    feat: 이미지가 있을 경우 카드 형식으로 나오게 class 적용

--- src/components/Home/HomeSectionCard.tsx
+++ src/components/Home/HomeSectionCard.tsx
@@ -2,26 +2,39 @@ import React from "react";
 import HomeSectionHeader from "./HomeSectionHeader";
 import { SectionType } from "./SectionType";
 import HomeSectionListItem from "./HomeSectionListItem";
-import { CommonPostItem } from "@/types/post";
+import { CommonPostItem, ImagePostItem } from "@/types/post";
+import PostImageCard from "../common/SectionCards/PostImageCard";
+import clsx from "clsx";
 
 interface HomeSectionProps {
   type: SectionType;
-  items: CommonPostItem[];
+  items: (CommonPostItem | ImagePostItem)[];
+  className?: string;
 }
 
-const HomeSectionCard = ({ type, items }: HomeSectionProps) => {
+const isImagePostItemArray = (
+  arr: (CommonPostItem | ImagePostItem)[],
+): arr is ImagePostItem[] => arr.length > 0 && "thumbnailUrl" in arr[0];
+
+const HomeSectionCard = ({ type, items, className }: HomeSectionProps) => {
+  const isImage = isImagePostItemArray(items);
+
   return (
-    <div>
+    <div className={clsx("w-full", className)}>
       <HomeSectionHeader type={type} />
-      <ul className="flex flex-col pt-3 gap-3 pl-4 pr-2">
-        {items.map((item) => (
-          <HomeSectionListItem
-            key={item.postId}
-            title={item.title}
-            timeAge={item.createAt}
-          />
-        ))}
-      </ul>
+      {isImage ? (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 pt-4">
+          {items.map((item) => (
+            <PostImageCard key={item.postId} {...item} />
+          ))}
+        </ul>
+      ) : (
+        <ul className="flex flex-col pt-3 gap-3 pl-4 pr-2">
+          {items.map((item) => (
+            <HomeSectionListItem key={item.postId} {...item} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
