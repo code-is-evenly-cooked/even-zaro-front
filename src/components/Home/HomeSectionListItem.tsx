170c44c83d3342108901345cc9bbf6f10a617476
seongmin <min@seongminui-MacBookPro.local>
2025-05-23 15:01:42 +0900

    feat: 리스트 아이템 데이터 형식 변경

--- src/components/Home/HomeSectionListItem.tsx
+++ src/components/Home/HomeSectionListItem.tsx
@@ -1,13 +1,10 @@
-interface HomeSectionListItemProps {
-  title: string;
-  timeAge: string;
-}
+import { CommonPostItem } from "@/types/post";
 
-const HomeSectionListItem = ({ title, timeAge }: HomeSectionListItemProps) => {
+const HomeSectionListItem = ({ title, createAt }: CommonPostItem) => {
   return (
     <li className="flex justify-between text-sm">
       <span className="text-gray900">{title}</span>
-      <span className="text-gray600">{timeAge}</span>
+      <span className="text-gray600">{createAt}</span>
     </li>
   );
 };
