53c8786eb8a28c96e447d399dd4f433c062178fa
seongmin <min@seongminui-MacBookPro.local>
2025-05-23 20:48:06 +0900

    feat: 카테고리 헤더 추가

--- src/components/PostList/PostListComponent.tsx
+++ src/components/PostList/PostListComponent.tsx
@@ -1,5 +1,6 @@
 import { MainCategory } from "@/types/category";
 import Searchbar from "../Searchbar/Searchbar";
+import PostListHeader from "./PostListHeader";
 
 interface PostListComponentProps {
   category: MainCategory;
@@ -9,6 +10,7 @@ const PostListComponent = ({ category }: PostListComponentProps) => {
   return (
     <div className="min-h-full flex flex-col pt-10 items-center px-4 max-w-3xl mx-auto">
       <Searchbar />
+      <PostListHeader category={category} />
     </div>
   );
 };
