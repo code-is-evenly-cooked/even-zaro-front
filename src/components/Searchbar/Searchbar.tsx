6c8544ed403ca292f0070d2636d61149f1c4c74f
seongmin <min@seongminui-MacBookPro.local>
2025-05-26 14:02:41 +0900

    feat: searchbar 디자인 수정

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import IconButton from "../common/Button/IconButton";
-import { RightArrowIcon } from "../common/Icons";
+import { SearchIcon } from "../common/Icons";
 import MainCategoryDropdown from "../Dropdown/MainCategoryDropdown";
 import useSearchbar from "./useSearchbar";
 
@@ -26,7 +26,7 @@ const Searchbar = () => {
       }}
       className="w-full max-w-full"
     >
-      <div className="relative flex gap-4">
+      <div className="relative flex gap-2">
         <MainCategoryDropdown
           selectedCategory={selectedCategory}
           isDropdownOpen={isDropdownOpen}
@@ -40,18 +40,19 @@ const Searchbar = () => {
         <div className="flex items-center bg-white border border-skyblue100 text-gray900 rounded-lg w-full">
           <input
             type="text"
-            className="flex-1 p-2 focus:outline-none focus:ring-0"
+            className="flex-1 px-4 py-2 focus:outline-none focus:ring-0"
             placeholder="검색어를 입력해주세요."
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
           />
           <IconButton
             type="submit"
-            icon={<RightArrowIcon />}
+            icon={<SearchIcon />}
             size="md"
-            color="skyblue300"
+            variant="icon"
             label="검색"
-            className="mr-2"
+            isTransparent
+            className="mx-2"
           />
         </div>
       </div>
