bf930fd825ce17bf4a1e6b9bce38c1e9e2cfe8f7
SangHwi <brilyent72@gmail.com>
2025-05-27 04:13:18 +0900

    feat: 게시글 좋아요 여부 조회 API 추가와 변경된 API 입력 방식에 맞게 전체 수정

--- src/lib/api/post.client.ts
+++ src/lib/api/post.client.ts
@@ -1,17 +1,32 @@
 import { client } from "@/lib/fetch/client";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+type LikeStatusResponse = {
+  code: string;
+  message: string;
+  data: boolean;
+};
+
+// 게시글 좋아요 여부 조회
+export const getPostLikeStatus = async (postId: number): Promise<boolean> => {
+  const res = await client<LikeStatusResponse>(
+    `/posts/${postId}/like`,
+    {
+      method: "GET",
+    },
+  );
+  return res.data;
+};
 
 // 게시글 좋아요
 export async function likePost(postId: number) {
-  return client(`${API_BASE_URL}/posts/${postId}/like`, {
+  return client(`/posts/${postId}/like`, {
     method: "POST",
   });
 }
 
 // 게시글 좋아요 취소
 export async function unlikePost(postId: number) {
-  return client(`${API_BASE_URL}/posts/${postId}/like`, {
+  return client(`/posts/${postId}/like`, {
     method: "DELETE",
   });
 }
