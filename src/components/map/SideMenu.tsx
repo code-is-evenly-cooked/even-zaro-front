35963f6c3fe87f43179e8e90f2ce9ec276b5378d
saaad99 <ehdgnstla@naver.com>
2025-05-27 17:34:53 +0900

    feat : 모달 여닫힘 구현

--- src/components/map/SideMenu.tsx
+++ src/components/map/SideMenu.tsx
@@ -1,5 +1,5 @@
 import { HomeIcon } from "lucide-react";
-import { Component } from "react";
+
 import {
   DefaultProfileIcon,
   LogoIcon,
@@ -7,23 +7,45 @@ import {
   TipIcon,
   TogetherIcon,
 } from "../common/Icons";
+import Link from "next/link";
+import { useState } from "react";
 
-export default class SideMenu extends Component {
-  render() {
-    return (
-      <div className="flex flex-nowrap w-24 h-auto justify-around p-4 absolute top-0 left-0 z-10 bg-white shadow-md">
-        <ul className="flex flex-col items-center justify-center space-y-6 w-full h-full ">
-          <li><LogoIcon className="w-16 h-16" /></li>
-          {/* 🔽 회색 구분선 추가 */}
-          <li className="w-3/4 border-t border-gray-300 my-2" />
+export default function SideMenu() {
+  const [openModal, setOpenModal] = useState(true);
 
-          <li><HomeIcon className="w-10 h-10" /></li>
-          <li><TogetherIcon className="w-10 h-10" /></li>
-          <li><TipIcon className="w-10 h-10" /></li>
-          <li><ShoppingBagIcon className="w-10 h-10" /></li>
-          <li><DefaultProfileIcon className="w-10 h-10 rounded-full object-cover"/></li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-nowrap w-24 h-auto justify-around p-4 absolute top-0 left-0 z-10 bg-white shadow-md">
+      <ul className="flex flex-col items-center justify-center space-y-6 w-full h-full">
+        {/*  메뉴 토글 버튼 (LogoIcon) */}
+        <li onClick={() => setOpenModal(!openModal)} className="cursor-pointer">
+          <LogoIcon className="w-16 h-16" />
+        </li>
+
+        {openModal && (
+          <>
+            {/* 🔽 회색 구분선 */}
+            <li className="w-3/4 border-t border-gray-300 my-2" />
+
+            <Link href="/">
+              <li>
+                <HomeIcon className="w-10 h-10" />
+              </li>
+            </Link>
+            <li>
+              <TogetherIcon className="w-10 h-10" />
+            </li>
+            <li>
+              <TipIcon className="w-10 h-10" />
+            </li>
+            <li>
+              <ShoppingBagIcon className="w-10 h-10" />
+            </li>
+            <li>
+              <DefaultProfileIcon className="w-10 h-10 rounded-full object-cover" />
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
 }
