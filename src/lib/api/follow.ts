e92fd36ad09ca2deb726a8742208ec1c63f711ee
SangHwi <brilyent72@gmail.com>
2025-05-27 15:38:22 +0900

    feat: 팔로우와 팔로우 취소 API 추가

--- src/lib/api/follow.ts
+++ src/lib/api/follow.ts
@@ -7,8 +7,24 @@ interface FollowingUser {
 }
 
 // 팔로잉 여부 조회
-export const fetchFollowings = async (userId: number): Promise<FollowingUser[]> => {
+export const fetchFollowings = async (
+  userId: number,
+): Promise<FollowingUser[]> => {
   return await client<FollowingUser[]>(`/profile/${userId}/followings`, {
     method: "GET",
+  });
+};
+
+// 팔로우
+export const followUser = async (userId: number) => {
+  await client(`/profile/${userId}/follow`, {
+    method: "POST",
+  });
+};
+
+// 팔로우 취소
+export const unfollowUser = async (userId: number) => {
+  await client(`/profile/${userId}/follow`, {
+    method: "DELETE",
   });
 };
