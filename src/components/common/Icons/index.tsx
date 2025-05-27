84270f3d115ba2a12073f1237da905be26be29ad
seongmin <min@seongminui-MacBookPro.local>
2025-05-27 19:12:29 +0900

    fix: 비밀번호 눈 아이콘 변경 및 기존 눈 아이콘 제거

--- src/components/common/Icons/index.tsx
+++ src/components/common/Icons/index.tsx
@@ -69,32 +69,6 @@ export const RightArrowIcon = ({ className }: IconProps) => {
   );
 };
 
-export const EyeIcon = ({ className }: IconProps) => {
-  return (
-    <Image
-      src="/icons/eyeOpen.svg"
-      alt="비밀번호 보기"
-      width={24}
-      height={24}
-      className={className}
-      priority
-    />
-  );
-};
-
-export const EyeCloseIcon = ({ className }: IconProps) => {
-  return (
-    <Image
-      src="/icons/eyeClose.svg"
-      alt="비밀번호 숨기기"
-      width={24}
-      height={24}
-      className={className}
-      priority
-    />
-  );
-};
-
 export const KakaoIcon = ({ className }: IconProps) => {
   return (
     <Image
