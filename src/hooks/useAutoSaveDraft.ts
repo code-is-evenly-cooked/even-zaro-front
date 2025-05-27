1f4afc515913e397ea4ea220f2ff87c2a2e07978
seongmin <min@seongminui-MacBookPro.local>
2025-05-27 16:29:52 +0900

    feat: 임시저장 diff있을 때만 호출하도록 변경

--- src/hooks/useAutoSaveDraft.ts
+++ src/hooks/useAutoSaveDraft.ts
@@ -1,30 +1,58 @@
-import { useEffect, MutableRefObject } from "react";
+import { useEffect, useRef, MutableRefObject } from "react";
 import { usePostStore } from "@/stores/usePostStore";
 import { Editor } from "@toast-ui/react-editor";
 import { saveDraft } from "@/utils/editorStorage";
+import { MainCategory, SubCategoryValue } from "@/types/category";
 
 export function useAutoSaveDraft(editorRef: MutableRefObject<Editor | null>) {
   const { title, mainCategory, subCategory } = usePostStore();
 
+  const prevDataRef = useRef<{
+    title: string;
+    mainCategory: MainCategory | null;
+    subCategory: SubCategoryValue | null;
+    content: string;
+  }>({
+    title: "",
+    mainCategory: null,
+    subCategory: null,
+    content: "",
+  });
+
   useEffect(() => {
     const interval = setInterval(() => {
       const content = editorRef.current?.getInstance().getMarkdown() ?? "";
-      console.log("💾 저장 시도 내용:", {
-        title,
-        mainCategory,
-        subCategory,
-        content,
-      });
-      saveDraft({
-        title,
-        mainCategory,
-        subCategory,
-        content,
-      });
-      console.log("자동 임시저장됨");
-    }, 5000); // 5초마다 자동 저장
+
+      const hasChanged =
+        prevDataRef.current.title !== title ||
+        prevDataRef.current.mainCategory !== mainCategory ||
+        prevDataRef.current.subCategory !== subCategory ||
+        prevDataRef.current.content !== content;
+
+      if (hasChanged) {
+        console.log("💾 변경 감지, 임시 저장:", {
+          title,
+          mainCategory,
+          subCategory,
+          content,
+        });
+
+        saveDraft({
+          title,
+          mainCategory,
+          subCategory,
+          content,
+        });
+
+        prevDataRef.current = {
+          title,
+          mainCategory,
+          subCategory,
+          content,
+        };
+      }
+    }, 10000);
 
     return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [title, mainCategory, subCategory]);
 }
