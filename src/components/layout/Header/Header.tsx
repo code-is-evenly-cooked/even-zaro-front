0a7a6ba2ad4b74fbae60240a87dfd9f94b4fc0b6
yoorym-kim <twilight99me@gmail.com>
2025-05-27 17:24:33 +0900

    feat : Header.tsx에서 useAuthStore 직후 useSse 호출

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -14,6 +14,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import useSse from "@/hooks/useSse";
 
 interface HeaderProps {
   onMenuClick: () => void;
@@ -34,6 +35,9 @@ const Header = ({ onMenuClick }: HeaderProps) => {
 
   const [isMobileSearchOpen, setIsMobileSearchOpen] = useState(false);
   const { user } = useAuthStore();
+
+  // SSE 연결 시도
+  useSse(user?.userId);
 
   useEffect(() => {
     const handleResize = () => {
