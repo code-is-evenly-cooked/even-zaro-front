e2adec4dad3cdf616d25cd52a7306841e1b0601e
saaad99 <ehdgnstla@naver.com>
2025-05-28 17:32:08 +0900

    feat : 아이콘에 툴팁 추가

--- src/components/map/SideMenu.tsx
+++ src/components/map/SideMenu.tsx
@@ -30,23 +30,46 @@ export default function SideMenu() {
             {/* 🔽 회색 구분선 */}
             <li className="w-3/4 border-t border-gray-300 my-2" />
 
-            <Link href="/">
-              <li>
-                <HomeIcon className="w-10 h-10" />
+            <button>
+              <Link href="/">
+                <li className="relative group">
+                  <HomeIcon className="w-10 h-10" />
+                  <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
+                    홈
+                  </div>
+                </li>
+              </Link>
+            </button>
+            <button>
+              <li className="relative group">
+                <TogetherIcon className="w-10 h-10" />
+                <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
+                  같이 쓰자
+                </div>
               </li>
-            </Link>
-            <li>
-              <TogetherIcon className="w-10 h-10" />
-            </li>
-            <li>
-              <TipIcon className="w-10 h-10" />
-            </li>
-            <li>
+            </button>
+            <button>
+              <li className="relative group">
+                <TipIcon className="w-10 h-10" />
+                <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
+                  자취 꿀팁
+                </div>
+              </li>
+            </button>
+            <li className="relative group">
               <ShoppingBagIcon className="w-10 h-10" />
+              <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
+                텅장 일기
+              </div>
             </li>
-            <li>
-              <DefaultProfileIcon className="w-10 h-10 rounded-full object-cover" />
-            </li>
+            <button>
+              <li className="relative group">
+                <DefaultProfileIcon className="w-10 h-10 rounded-full object-cover" />
+                <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
+                  내 프로필
+                </div>
+              </li>
+            </button>
           </>
         )}
       </ul>
