c0e1b7ab6028ee51073ba8c19619e9dd352d4feb
yoorym-kim <twilight99me@gmail.com>
2025-05-29 21:11:38 +0900

    feat : NotificationHeader 기본 구조

---
+++ src/components/notification/NotificationHeader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const NotificationHeader = () => {
+  return (
+    <div>
+      <span>알림</span>
+      <button>전체 읽기</button>
+    </div>
+  );
+};
+
+export default NotificationHeader;
