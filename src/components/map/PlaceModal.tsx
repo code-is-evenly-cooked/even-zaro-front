b9a79482d343d4d82cd0587f0e311d679621387a
saaad99 <ehdgnstla@naver.com>
2025-05-29 16:06:46 +0900

    feat : api 응답객체와 같은 형식으로 수정

--- src/components/map/PlaceModal.tsx
+++ src/components/map/PlaceModal.tsx
@@ -3,34 +3,48 @@ import PlaceCard from "@/components/map/PlaceCard";
 
 export default function PlaceModal() {
 
-  const dummyData = [
+  const dummyData = {
+    "totalCount" : 5,
+    "placeInfos": [
     {
-      "placeId" : 1,
-      "placeName" : "갓덴 스시 강남점",
-      "address" : "서울의 어딘가 123",
-      "favoriteCount" : 10
+      "place_id": 1,
+      "placeName": "갓덴 스시 강남점",
+      "address": "서울의 어딘가 123",
+      "favoriteCount": 10,
+      "lat": 36.123,
+      "lng": 127.232
     },
     {
-      "placeId" : 2,
-      "placeName" : "신동궁 감자탕 역삼직영",
-      "address" : "서울의 어딘가 1234",
-      "favoriteCount" : 5
-    },{
-      "placeId" : 3,
-      "placeName" : "장인 닭갈비 강남점",
-      "address" : "서울의 어딘가 123 345",
-      "favoriteCount" : 12
-    },{
-      "placeId" : 4,
-      "placeName" : "장소1",
-      "address" : "서울의 어딘가 123 3211",
-      "favoriteCount" : 11
-    }
+      "place_id": 2,
+      "placeName": "신동궁 감자탕 역삼직영",
+      "address": "서울의 어딘가 1234",
+      "favoriteCount": 5,
+      "lat": 36.123,
+      "lng": 127.232
+    },
+    {
+    "place_id": 3,
+    "placeName": "장인 닭갈비 강남점",
+    "address": "서울의 어딘가 123 345",
+    "favoriteCount": 12,
+    "lat": 36.123,
+    "lng": 127.232
+  },
+  {
+    "place_id": 4,
+    "placeName": "장소1",
+    "address": "서울의 어딘가 123 3211",
+    "favoriteCount": 11,
+    "lat": 36.123,
+    "lng": 127.232
+  }
   ]
+};
 
 
   return (
-    <div className="flex flex-col absolute -bottom-4 left-4 z-10 w-96 h-96 bg-white rounded-t-2xl shadow-lg overflow-hidden">
+    <div
+      className="flex flex-col absolute -bottom-4 left-4 z-10 w-96 h-96 bg-white rounded-t-2xl shadow-lg overflow-hidden">
       {/* 헤더 */}
       <div className="px-4 pt-4">
         <span className="text-2xl font-extrabold block ">서울시 &gt; 강남구</span>
@@ -41,9 +55,9 @@ export default function PlaceModal() {
 
       {/* 장소 카드 리스트 */}
       <div className="flex flex-col gap-3 px-4 py-4 overflow-y-auto">
-        {dummyData.map((place) => (
+        {dummyData.placeInfos.map((place) => (
           <PlaceCard
-            key={place.placeId}
+            key={place.place_id}
             placeName={place.placeName}
             address={place.address}
             favoriteCount={place.favoriteCount}
