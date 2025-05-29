e6f38af0268e7875062812eed791116fcbdb1263
SangHwi <brilyent72@gmail.com>
2025-05-30 03:18:24 +0900

    feat: 프로필 page.tsx 에서 탭 선택 정보를 전달하도록 수정

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { useState } from "react";
 import ProfileHeader from "@/components/Profile/ProfileHeader";
 import ProfileTabs from "@/components/Profile/ProfileTabs";
 import ProfileTabContent from "@/components/Profile/ProfileTabContent";
+import { ProfileTabType } from "@/types/profile";
 
 export default function ProfilePage() {
+    const [activeTab, setActiveTab] = useState<ProfileTabType>("bookmarks");
   return (
     <div className="max-w-5xl mx-auto px-4 py-8">
       <ProfileHeader />
       <div className="flex flex-col max-w-3xl mx-auto">
-        <ProfileTabs />
-        <ProfileTabContent />
+      <ProfileTabs activeTab={activeTab} onTabChange={setActiveTab} />
+      <ProfileTabContent activeTab={activeTab} />
       </div>
     </div>
   );
