6bca479463004040e16a28a353b19903c2684476
saaad99 <ehdgnstla@naver.com>
2025-05-29 19:36:23 +0900

    feat : 카테고리 필드 추가

--- src/components/map/PlaceCard.tsx
+++ src/components/map/PlaceCard.tsx
@@ -1,15 +1,41 @@
+import { Food, Cafe, Etc, Market, Favorite } from "@/components/common/Icons/category";
+import { JSX } from "react";
+
 interface PlaceCardProps {
   placeName: string;
   address: string;
+  category: string;
   favoriteCount: number;
 }
 
-export default function PlaceCard({ placeName, address, favoriteCount }: PlaceCardProps) {
+const categoryIcons: Record<string, JSX.Element> = {
+  Food: <Food />,
+  Cafe: <Cafe />,
+  Market: <Market />,
+  Etc : <Etc />,
+  Favorite : <Favorite />
+  // Hospital: <Hospital />,
+  // Add more category-icon pairs as needed
+};
+
+
+export default function PlaceCard({
+  placeName,
+  address,
+  category,
+  favoriteCount,
+}: PlaceCardProps) {
   return (
-    <div className="p-3 shadow-sm hover:bg-gray-100 transition">
-      <h3 className="font-bold text-base">{placeName}</h3>
-      <p className="text-sm text-gray-500">{address}</p>
-      <p className="text-sm text-gray-400">즐겨찾기 {favoriteCount}개</p>
+    <div className="flex items-center">
+      {/* 이미지 영역 */}
+      <div className="flex w-14 h-14 items-center justify-center mr-4 border border-gray-300 rounded-full">
+        {categoryIcons[category] || <Etc />}
+      </div>
+      <div className="p-3 shadow-sm hover:bg-gray-100 transition">
+        <h3 className="font-bold text-base">{placeName}</h3>
+        <p className="text-sm text-gray-500">{address}</p>
+        <p className="text-sm text-gray-400">즐겨찾기 {favoriteCount}개</p>
+      </div>
     </div>
   );
 }
--- src/components/map/PlaceModal.tsx
+++ src/components/map/PlaceModal.tsx
@@ -2,52 +2,64 @@ import React from "react";
 import PlaceCard from "@/components/map/PlaceCard";
 
 export default function PlaceModal() {
-
   const dummyData = {
-    "totalCount" : 5,
-    "placeInfos": [
-    {
-      "place_id": 1,
-      "placeName": "갓덴 스시 강남점",
-      "address": "서울의 어딘가 123",
-      "favoriteCount": 10,
-      "lat": 36.123,
-      "lng": 127.232
-    },
-    {
-      "place_id": 2,
-      "placeName": "신동궁 감자탕 역삼직영",
-      "address": "서울의 어딘가 1234",
-      "favoriteCount": 5,
-      "lat": 36.123,
-      "lng": 127.232
-    },
-    {
-    "place_id": 3,
-    "placeName": "장인 닭갈비 강남점",
-    "address": "서울의 어딘가 123 345",
-    "favoriteCount": 12,
-    "lat": 36.123,
-    "lng": 127.232
-  },
-  {
-    "place_id": 4,
-    "placeName": "장소1",
-    "address": "서울의 어딘가 123 3211",
-    "favoriteCount": 11,
-    "lat": 36.123,
-    "lng": 127.232
-  }
-  ]
-};
-
+    totalCount: 5,
+    placeInfos: [
+      {
+        place_id: 1,
+        placeName: "갓덴 스시 강남점",
+        address: "서울의 어딘가 123",
+        category: "Food",
+        favoriteCount: 10,
+        lat: 36.123,
+        lng: 127.232,
+      },
+      {
+        place_id: 2,
+        placeName: "신동궁 감자탕 역삼직영",
+        address: "서울의 어딘가 1234",
+        category: "Cafe",
+        favoriteCount: 5,
+        lat: 36.123,
+        lng: 127.232,
+      },
+      {
+        place_id: 3,
+        placeName: "장인 닭갈비 강남점",
+        address: "서울의 어딘가 123 345",
+        category: "Etc",
+        favoriteCount: 12,
+        lat: 36.123,
+        lng: 127.232,
+      },
+      {
+        place_id: 4,
+        placeName: "장소1",
+        address: "서울의 어딘가 123 3211",
+        category: "Market",
+        favoriteCount: 11,
+        lat: 36.123,
+        lng: 127.232,
+      },
+      {
+        place_id: 5,
+        placeName: "장소2",
+        address: "서울의 어딘가 123 3211",
+        category: "Favorite",
+        favoriteCount: 5,
+        lat: 36.123,
+        lng: 127.232,
+      },
+    ],
+  };
 
   return (
-    <div
-      className="flex flex-col absolute -bottom-4 left-4 z-10 w-96 h-96 bg-white rounded-t-2xl shadow-lg overflow-hidden">
+    <div className="flex flex-col absolute -bottom-4 left-4 z-10 w-96 h-96 bg-white rounded-t-2xl shadow-lg overflow-hidden">
       {/* 헤더 */}
       <div className="px-4 pt-4">
-        <span className="text-2xl font-extrabold block ">서울시 &gt; 강남구</span>
+        <span className="text-2xl font-extrabold block ">
+          서울시 &gt; 강남구
+        </span>
         <div className="flex justify-end">
           <span className="text-sm text-gray-400">즐겨찾기 많은 순</span>
         </div>
@@ -58,6 +70,7 @@ export default function PlaceModal() {
         {dummyData.placeInfos.map((place) => (
           <PlaceCard
             key={place.place_id}
+            category={place.category}
             placeName={place.placeName}
             address={place.address}
             favoriteCount={place.favoriteCount}
