aa1fcf0662f533c137de3092115e72bb34fccb52
seongmin <min@Seongminui-MacBookPro-2.local>
2025-05-29 11:23:52 +0900

    feat: 검색에 Suspense, ErrorBoundry적용

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,35 +1,27 @@
+import AppErrorBoundary from "@/components/common/ErrorBoundary/ErrorBoundary";
+import LoadingSpinnerBoundary from "@/components/common/LoadingSpinner/LoadingSpinnerBoundary";
+
 import SearchResult from "@/components/Search/SearchResult";
 import Searchbar from "@/components/Searchbar/Searchbar";
-import { server } from "@/lib/fetch/server";
-
-import { PostDetailResponse } from "@/types/post";
-import React from "react";
+import { Suspense } from "react";
 
 interface PageProps {
   searchParams: Promise<{
     keyword: string;
-    category?: string;
-    tag?: string;
-    page?: string;
   }>;
 }
 
 const SearchPage = async ({ searchParams }: PageProps) => {
-  const { keyword, category, tag, page } = await searchParams;
+  const { keyword } = await searchParams;
 
-  const posts = await server<PostDetailResponse>(`/search/es`, {
-    needAuth: true,
-    params: {
-      keyword,
-      ...(category ? { category } : {}),
-      ...(tag ? { tag } : {}),
-      page: Number(page) || 0,
-    },
-  });
   return (
     <div className="flex flex-col w-full max-w-4xl mx-auto mt-10">
       <Searchbar />
-      <SearchResult keyword={keyword} initialData={posts} />
+      <AppErrorBoundary fallbackMessage="검색 결과가 없습니다." key={keyword}>
+        <Suspense fallback={<LoadingSpinnerBoundary />}>
+          <SearchResult keyword={keyword} />
+        </Suspense>
+      </AppErrorBoundary>
     </div>
   );
 };
