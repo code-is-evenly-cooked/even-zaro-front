272d0627caaa10d3742caf2e5fd79173e471043e
seongmin <min@Seongminui-MacBookPro-2.local>
2025-05-29 20:49:00 +0900

    feat: 이메일 미인증 유저의 경우 이메일 인증 하도록 유도

--- src/components/PostList/PostListHeader.tsx
+++ src/components/PostList/PostListHeader.tsx
@@ -1,21 +1,49 @@
 "use client";
 
+import { useAuthStore } from "@/stores/useAuthStore";
 import { MainCategory } from "@/types/category";
 import { getMainCategoryTitle } from "@/utils/category";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import EmailValidateModal from "../common/EmailValidate/EmailValidateModal";
 
 interface PostListHeaderProps {
   category: MainCategory;
 }
 const PostListHeader = ({ category }: PostListHeaderProps) => {
+  const router = useRouter();
+  const { user } = useAuthStore();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleWritePostClick = () => {
+    if (!user || !user.isValidated) {
+      setModalOpen(true);
+    } else {
+      router.push(`/editor?category=${category}`);
+    }
+  };
+
+  const handleEmailValidateClick = (email?: string) => {
+    setModalOpen(false);
+    if (email) {
+      router.push(`/email-validation?email=${email}`);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between pt-10">
       <h1 className="text-3xl font-bold">{getMainCategoryTitle(category)}</h1>
-      <Link href={{ pathname: "/editor", query: { category } }}>
-        <span className="border border-gray200 hover:bg-gray100 px-3 py-2.5 rounded-md text-gray900 text-md">
-          글쓰기
-        </span>
-      </Link>
+
+      <button
+        className="border border-gray200 hover:bg-gray100 px-3 py-2.5 rounded-md text-gray900 text-md"
+        onClick={handleWritePostClick}
+      >
+        글쓰기
+      </button>
+      <EmailValidateModal
+        isOpen={modalOpen}
+        onClose={handleEmailValidateClick}
+      />
     </div>
   );
 };
