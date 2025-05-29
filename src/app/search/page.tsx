2677b12d36006c5dae2b623132f5f67f9b4e82fe
seongmin <min@Seongminui-MacBookPro-2.local>
2025-05-29 13:52:29 +0900

    fix: category, tag 변경 시 컴포넌트 다시 마운트

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,19 +3,20 @@ import LoadingSpinnerBoundary from "@/components/common/LoadingSpinner/LoadingSp
 
 import SearchResult from "@/components/Search/SearchResult";
 import Searchbar from "@/components/Searchbar/Searchbar";
-import { MainCategory } from "@/types/category";
+import { MainCategory, SubCategoryValue } from "@/types/category";
 import { Suspense } from "react";
 
 interface PageProps {
   searchParams: Promise<{
     keyword: string;
     category?: MainCategory;
+    tag?: SubCategoryValue;
     onlyTag?: boolean;
   }>;
 }
 
 const SearchPage = async ({ searchParams }: PageProps) => {
-  const { keyword, category, onlyTag } = await searchParams;
+  const { keyword, category, tag, onlyTag } = await searchParams;
 
   return (
     <div className="flex flex-col w-full max-w-4xl mx-auto mt-10">
@@ -24,7 +25,10 @@ const SearchPage = async ({ searchParams }: PageProps) => {
         mainCategory={category}
         onlyTag={onlyTag}
       />
-      <AppErrorBoundary fallbackMessage="검색 결과가 없습니다." key={keyword}>
+      <AppErrorBoundary
+        fallbackMessage="검색 결과가 없습니다."
+        key={`${keyword}-${category ?? "all"}-${tag ?? "all"}`}
+      >
         <Suspense fallback={<LoadingSpinnerBoundary />}>
           <SearchResult keyword={keyword} />
         </Suspense>
