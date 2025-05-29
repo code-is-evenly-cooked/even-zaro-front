deeeba48ab8329580dc73e83f4652d8cf2120c6a
yoorym-kim <twilight99me@gmail.com>
2025-05-29 22:25:45 +0900

    feat : 임시 더미데이터 추가

--- src/components/notification/NotificationModal.tsx
+++ src/components/notification/NotificationModal.tsx
@@ -4,6 +4,38 @@ import NotificationHeader from "./NotificationHeader";
 import NotificationItem from "./NotificationItem";
 
 const NotificationModal = () => {
+  const dummyNotifications = [
+    {
+      id: 4,
+      type: "COMMENT" as const,
+      username: "냉장고요정",
+      createdAt: "3분 전",
+      comment: "이건 진짜 꿀템이네요!",
+      thumbnailImage: "/images/post/uuid4.png",
+    },
+    {
+      id: 3,
+      type: "LIKE" as const,
+      username: "자취왕",
+      createdAt: "7분 전",
+      thumbnailImage: "/images/post/uuid3.png",
+    },
+    {
+      id: 2,
+      type: "FOLLOW" as const,
+      username: "맛잘알",
+      createdAt: "15분 전",
+    },
+    {
+      id: 1,
+      type: "COMMENT" as const,
+      username: "혼밥천재",
+      createdAt: "30분 전",
+      comment: "레시피 공유 가능할까요?",
+      thumbnailImage: "/images/post/uuid1.png",
+    },
+  ];
+
   return (
     <div className="w-[360px] h-[360px] bg-white z-50 border border-gray-200">
       <header>
@@ -11,7 +43,7 @@ const NotificationModal = () => {
       </header>
       <div>
         {/* map으로 돌려야함 */}
-        <NotificationItem type="LIKE" />
+        <NotificationItem type="COMMENT" />
       </div>
     </div>
   );
