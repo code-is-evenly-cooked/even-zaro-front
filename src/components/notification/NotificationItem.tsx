c8999dd10ea08565f5d463d5a153241652534017
yoorym-kim <twilight99me@gmail.com>
2025-05-29 21:26:49 +0900

    feat : NotificationItem 기본 레이아웃

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -1,7 +1,24 @@
 "use client";
 
 const NotificationItem = () => {
-  return <div></div>;
+  return (
+    <div>
+      {/* 읽음표시 img */}
+      <img />
+      <div>
+        {/* 알림액션 한 상대유저의 프로필이미지 */}
+        <img />
+        <div>
+          <div>
+            <span>알림 내용</span>
+            <span>알림 생성 시각</span>
+          </div>
+          {/* 게시물 썸네일 img (게시물 좋아요일때만) */}
+          <img />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotificationItem;
