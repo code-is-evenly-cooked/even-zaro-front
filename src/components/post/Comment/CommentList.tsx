ba83ae0982a113127a7109c9a032a792ee67ac8d
seongmin <min@Seongminui-MacBookPro-2.local>
2025-05-30 15:55:04 +0900

    feat: CommentList 실제 데이터 모델 주입

--- src/components/post/Comment/CommentList.tsx
+++ src/components/post/Comment/CommentList.tsx
@@ -1,33 +1,17 @@
 "use client";
-import { CommentResponse } from "@/types/comment";
-import React from "react";
+import { CommentItem } from "@/types/comment";
 import CommentListItem from "./CommentListItem";
 import IconButton from "@/components/common/Button/IconButton";
 import { RefreshCwIcon } from "lucide-react";
 
-// TODO: 지우기
-const dummy: CommentResponse = {
-  content: [
-    {
-      id: 1,
-      content: "너무 좋네요!!",
-      nickname: "test003",
-      profileImage: null,
-      liveAloneDate: null,
-      createdAt: "2025-05-29T12:07:11.013407",
-      updatedAt: "2025-05-29T12:07:11.013443",
-      isEdited: false,
-      isMine: true,
-      mentionedUser: null,
-    },
-  ],
-  totalPages: 1,
-  number: 0,
-};
+interface CommentListProps {
+  comments: CommentItem[];
+  refresh: () => void;
+}
 
-const CommentList = () => {
+const CommentList = ({ comments, refresh }: CommentListProps) => {
   const handleRefresh = () => {
-    console.log("refresh");
+    refresh();
   };
 
   return (
@@ -42,12 +26,12 @@ const CommentList = () => {
         />
       </div>
       <ul className="flex flex-col mx-2 py-2">
-        {dummy.content.map((comment, index) => (
+        {comments.map((comment, index) => (
           <li key={comment.id}>
             <CommentListItem
               key={comment.id}
               item={comment}
-              isLast={index === dummy.content.length - 1}
+              isLast={index === comments.length - 1}
             />
           </li>
         ))}
