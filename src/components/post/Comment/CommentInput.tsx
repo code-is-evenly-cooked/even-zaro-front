8e5b988f9c827edb466643fbd72f9040331cc597
seongmin <min@Seongminui-MacBookPro-2.local>
2025-05-30 16:39:46 +0900

    feat: 사용자 멘션 추가

--- src/components/post/Comment/CommentInput.tsx
+++ src/components/post/Comment/CommentInput.tsx
@@ -1,16 +1,43 @@
 "use client";
 
 import { client } from "@/lib/fetch/client";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 
 interface CommentInputProps {
   postId: number;
-  isReply: boolean;
+  replyNickname?: string;
   onSuccess: () => void;
 }
 
-const CommentInput = ({ postId, onSuccess }: CommentInputProps) => {
+const CommentInput = ({
+  postId,
+  replyNickname,
+  onSuccess,
+}: CommentInputProps) => {
   const [comment, setComment] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const mention = replyNickname ? `@${replyNickname} ` : "";
+
+  useEffect(() => {
+    if (replyNickname) setComment(mention);
+  }, [replyNickname]);
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newValue = e.target.value;
+    const cursorPos = e.target.selectionStart;
+
+    // mention이 있고, 커서가 mention안에 있고, 삭제중이면 전체 mention제거
+    if (
+      replyNickname &&
+      comment.startsWith(mention) &&
+      cursorPos <= mention.length &&
+      newValue.length < comment.length
+    ) {
+      setComment("");
+    } else {
+      setComment(newValue);
+    }
+  };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,7 +46,13 @@ const CommentInput = ({ postId, onSuccess }: CommentInputProps) => {
     await client(`/posts/${postId}/comments`, {
       method: "POST",
       needAuth: true,
-      body: JSON.stringify({ content: comment, mentionedNickname: "" }),
+      body: JSON.stringify({
+        content: comment,
+        mentionedNickname:
+          replyNickname && comment.startsWith(`@${replyNickname}`)
+            ? replyNickname
+            : "",
+      }),
     });
 
     setComment("");
@@ -31,8 +64,9 @@ const CommentInput = ({ postId, onSuccess }: CommentInputProps) => {
       <p className="px-2 font-semibold ">댓글 쓰기</p>
       <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
         <textarea
+          ref={textareaRef}
           value={comment}
-          onChange={(e) => setComment(e.target.value)}
+          onChange={handleChange}
           placeholder="댓글을 입력하세요"
           className="w-full h-24 p-3 bg-gray100 rounded-b-xl overflow-y-auto resize-none focus:outline-none
           focus:ring-0 text-md"
@@ -54,4 +88,6 @@ export default CommentInput;
 /**
  * TODO
  * 답글 기능일때 UI를 어떻게 표시할것인가?
+ * 글자 제한
+ * 수정...
  */
