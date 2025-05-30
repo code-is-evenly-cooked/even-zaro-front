d195bd56081c1cf1447e4e7ddf79624b7a7b68d3
saaad99 <ehdgnstla@naver.com>
2025-05-30 16:36:55 +0900

    feat : 유저 프로필 이미지 크기 조정

--- src/components/map/UserMemoCard.tsx
+++ src/components/map/UserMemoCard.tsx
@@ -4,8 +4,8 @@ export default function UserMemoCard() {
   return (
     <div className="flex items-center">
       {/* 이미지 영역 */}
-      <div className="flex w-14 h-14 items-center justify-center mr-4 border border-gray-300 rounded-full">
-        <SampleProfile />
+      <div className="flex items-center justify-center rounded-full w-10 h-10 border-2 border-gray200">
+        <SampleProfile className="rounded-full w-10 border-1 border-gray200" />
       </div>
       <div className="flex-1 p-3 shadow-sm hover:bg-gray-100 transition">
         <h3 className="font-bold text-base ">이브니</h3>
