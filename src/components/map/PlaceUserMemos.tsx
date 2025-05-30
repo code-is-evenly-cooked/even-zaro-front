9209c13d921cf0385e456c92ae627ac9df93bfa4
saaad99 <ehdgnstla@naver.com>
2025-05-30 17:43:58 +0900

    feat : 더보기, 즐겨찾기 <button> 태그로 수정

--- src/components/map/PlaceUserMemos.tsx
+++ src/components/map/PlaceUserMemos.tsx
@@ -1,37 +1,37 @@
 import React from "react";
-import { DefaultProfileIcon, MoreIcon, SampleProfile, StarIcon } from "@/components/common/Icons";
+import { MoreIcon, StarIcon } from "@/components/common/Icons";
 import UserMemoCard from "@/components/map/UserMemoCard";
 
 export default function PlaceUserMemos() {
   const dummyData = {
-    placeId : 1,
-    placeName : "갓덴 스시 강남점",
-    address : "서울의 어딘가 123",
+    placeId: 1,
+    placeName: "갓덴 스시 강남점",
+    address: "서울의 어딘가 123",
     lat: 36.123,
     lng: 127.232,
     favoriteCount: 5,
     userInfo: [
       {
-        userId : 1,
-        profileImage : "/icons/defaultProfile.svg",
+        userId: 1,
+        profileImage: "/icons/defaultProfile.svg",
         nickname: "이브니",
         memo: "요기 맛없어요asdsadasdasdasdasasdasd1",
       },
       {
-        userId : 2,
-        profileImage : "/icons/sampleProfile.svg",
+        userId: 2,
+        profileImage: "/icons/sampleProfile.svg",
         nickname: "삼브니",
         memo: "요기 맛없어asdasdasdsalkdjaslkdjaslkdjalskj2",
       },
       {
-        userId : 3,
-        profileImage : "/icons/sampleProfile.svg",
+        userId: 3,
+        profileImage: "/icons/sampleProfile.svg",
         nickname: "사브니",
         memo: "요기 맛없어요3",
       },
       {
-        userId : 4,
-        profileImage : "/icons/sampleProfile.svg",
+        userId: 4,
+        profileImage: "/icons/sampleProfile.svg",
         nickname: "오브니",
         memo: "요기 맛없어요4",
       },
@@ -40,13 +40,15 @@ export default function PlaceUserMemos() {
   return (
     <div className="flex flex-col absolute left-[400px] -bottom-4 z-10 w-96 h-96 bg-white rounded-t-2xl shadow-lg overflow-hidden">
       <div className="relative w-full px-4 py-4">
-        <div className="absolute top-4 right-4">
-          <MoreIcon className="w-5 h-5 text-gray-500 mt-0.5" />
-        </div>
+        <button className="absolute top-4 right-4 w-5 h-5 mt-0.5">
+          <MoreIcon />
+        </button>
 
         <div className="flex flex-col items-center justify-center">
           <div className="flex items-center space-x-2">
-            <StarIcon className="flex self-start mt-0.5 w-5 h-5 text-gray-700" />
+            <button className="flex self-start mt-0.5 w-5 h-5">
+              <StarIcon />
+            </button>
 
             <div className="flex flex-col justify-center">
               <span className="font-bold text-gray-900 text-lg leading-snug">
@@ -69,11 +71,16 @@ export default function PlaceUserMemos() {
         ) : (
           <>
             <div className="flex -space-x-6">
-              {dummyData.userInfo.slice(0,3).map((user, idx) => (
-                <div className="flex items-center justify-center rounded-full w-10 h-10 border-2 border-gray200"
-                  key={idx}>
-                  <img className="rounded-full w-10 h-10 border-1 border-gray200 flex-shrink-0"
-                    src={user.profileImage} alt="유저 이미지"/>
+              {dummyData.userInfo.slice(0, 3).map((user, idx) => (
+                <div
+                  className="flex items-center justify-center rounded-full w-10 h-10 border-2 border-gray200"
+                  key={idx}
+                >
+                  <img
+                    className="rounded-full w-10 h-10 border-1 border-gray200 flex-shrink-0"
+                    src={user.profileImage}
+                    alt="유저 이미지"
+                  />
                 </div>
               ))}
             </div>
@@ -81,7 +88,10 @@ export default function PlaceUserMemos() {
               <span className="font-bold">
                 {dummyData.userInfo[0].nickname}
               </span>
-              <span> 님 외 {dummyData.favoriteCount} 명이 즐겨찾기에 추가했습니다.</span>
+              <span>
+                {" "}
+                님 외 {dummyData.favoriteCount} 명이 즐겨찾기에 추가했습니다.
+              </span>
             </div>
           </>
         )}
