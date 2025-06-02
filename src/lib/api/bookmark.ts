2ca4cc199b4800f5327d3ccbbc4b502d8416ed52
SangHwi <brilyent72@gmail.com>
2025-06-02 23:14:24 +0900

    feat: 즐겨찾기 그룹 삭제 API 추가

--- src/lib/api/bookmark.ts
+++ src/lib/api/bookmark.ts
@@ -10,3 +10,10 @@ export const updateBookmarkGroupName = async (
     body: JSON.stringify({ groupName }),
   });
 };
+
+// 그룹 삭제
+export const deleteBookmarkGroup = async (groupId: number): Promise<void> => {
+  return await client(`/group/${groupId}`, {
+    method: "DELETE",
+  });
+};
