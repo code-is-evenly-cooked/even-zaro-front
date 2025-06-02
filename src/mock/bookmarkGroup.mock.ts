191e8343e0f3d132c71e38b783771cc83ad3cf4f
SangHwi <brilyent72@gmail.com>
2025-06-02 11:13:38 +0900

    feat: 즐겨찾기 그룹 응답 데이터에 기반한 목업 파일 생성

---
+++ src/mock/bookmarkGroup.mock.ts
@@ -0,0 +1,20 @@
+import type { BookmarkGroupType } from "@/types/bookmark";
+
+export const mockBookmarkGroups: BookmarkGroupType[] = [
+  {
+    userId: 5,
+    groupId: 1,
+    name: "성수동 힙하고 멋진 카페",
+    isDeleted: false,
+    createdAt: "2025-05-19T16:02:35.735757",
+    updatedAt: "2025-05-19T16:03:11.202026",
+  },
+  {
+    userId: 5,
+    groupId: 2,
+    name: "연남동 따뜻한 분위기 카페",
+    isDeleted: false,
+    createdAt: "2025-05-19T18:07:50.148227",
+    updatedAt: "2025-05-19T18:07:50.148249",
+  },
+];
