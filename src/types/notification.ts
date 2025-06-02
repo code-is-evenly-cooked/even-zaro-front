906ea068105393883eba31ecaffefd23f2141898
yoorym-kim <twilight99me@gmail.com>
2025-06-02 14:47:03 +0900

    feat : 알림아이템 인터페이스 정의

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -1,1 +1,15 @@
 export type NotificationType = "LIKE" | "FOLLOW" | "COMMENT";
+
+export interface NotificationItem {
+  id: number;
+  type: NotificationType;
+  targetId: number;
+  createdAt: string;
+  userId: number;
+  username: string;
+  profileImage: string | null;
+  category: string | null;
+  thumbnailImage: string | null;
+  comment: string | null;
+  read: boolean;
+}
