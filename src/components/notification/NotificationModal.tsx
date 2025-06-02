76dc5f333c8ff3f9d63a98de2c88bcce0e88e03f
yoorym-kim <twilight99me@gmail.com>
2025-06-02 15:33:09 +0900

    ref : 더미데이터 삭제

--- src/components/notification/NotificationModal.tsx
+++ src/components/notification/NotificationModal.tsx
@@ -28,67 +28,6 @@ const NotificationModal = ({ onClose }: NotificationModalProps) => {
 
     fetchData();
   }, []);
-  // const dummyNotifications = [
-  //   {
-  //     id: 5,
-  //     type: "LIKE" as const,
-  //     username: "아아",
-  //     userId: 23,
-  //     profileImage: null,
-  //     createdAt: "2025-05-30T12:34:56",
-  //     //   thumbnailImage: "/images/post/uuid3.png",
-  //     isRead: true,
-  //     category: "TOGETHER" as MainCategory,
-  //     postId: 2,
-  //   },
-  //   {
-  //     id: 4,
-  //     type: "COMMENT" as const,
-  //     username: "냉장고요정",
-  //     userId: 12,
-  //     profileImage: null,
-  //     createdAt: "2025-05-28T12:34:56",
-  //     comment: "이건 진짜 꿀템이네요!",
-  //     thumbnailImage: "/images/post/uuid4.png",
-  //     isRead: false,
-  //     category: "TOGETHER" as MainCategory,
-  //     postId: 2,
-  //   },
-  //   {
-  //     id: 3,
-  //     type: "LIKE" as const,
-  //     username: "자취왕",
-  //     userId: 9,
-  //     profileImage: null,
-  //     createdAt: "2025-05-21T12:34:56",
-  //     //   thumbnailImage: "/images/post/uuid3.png",
-  //     isRead: true,
-  //     category: "DAILY_LIFE" as MainCategory,
-  //     postId: 5,
-  //   },
-  //   {
-  //     id: 2,
-  //     type: "FOLLOW" as const,
-  //     username: "맛잘알",
-  //     userId: 5,
-  //     profileImage: null,
-  //     createdAt: "2025-05-21T12:34:56",
-  //     isRead: true,
-  //   },
-  //   {
-  //     id: 1,
-  //     type: "COMMENT" as const,
-  //     username: "혼밥천재",
-  //     userId: 88,
-  //     profileImage: null,
-  //     createdAt: "2025-05-21T12:34:56",
-  //     comment: "레시피 공유 가능할까요?",
-  //     thumbnailImage: "2025-05-21T12:34:56",
-  //     isRead: false,
-  //     category: "RANDOM_BUY" as MainCategory,
-  //     postId: 8,
-  //   },
-  // ];
 
   return (
     <div className="w-[420px] h-[360px] bg-white z-50 border-t border-gray-100 rounded-xl shadow-md p-1 overflow-hidden">
