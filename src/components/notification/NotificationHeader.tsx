bd6ce34c086177657793d38a52b1aad93122b342
yoorym-kim <twilight99me@gmail.com>
2025-06-03 11:32:25 +0900

    style : 전체읽기 버튼 호버 시 투명도 조절

--- src/components/notification/NotificationHeader.tsx
+++ src/components/notification/NotificationHeader.tsx
@@ -9,7 +9,7 @@ const NotificationHeader = ({ onMarkAllRead }: NotificationHeaderProps) => {
     <div className="flex items-center justify-between h-[50px] px-4 py-2 border-b border-gray-200">
       <span className="text-base ml-1 pt-2">알림</span>
       <button
-        className="text-sm font-semibold text-gray-400 hover:text-gray-600 pt-2 mr-1"
+        className="text-sm font-semibold text-gray-400 hover:text-gray-400/80 pt-2 mr-1"
         onClick={onMarkAllRead}
       >
         전체 읽기
