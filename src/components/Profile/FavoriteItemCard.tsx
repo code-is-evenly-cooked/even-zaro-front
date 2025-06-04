1304b483d56af6f06bfadcc298941c283135b755
SangHwi <brilyent72@gmail.com>
2025-06-04 13:52:07 +0900

    feat: 임시 프로필 카드 설정 및 디자인

--- src/components/Profile/FavoriteItemCard.tsx
+++ src/components/Profile/FavoriteItemCard.tsx
@@ -7,9 +7,12 @@ interface Props {
 export default function FavoriteItemCard({ item }: Props) {
   return (
     <li className="p-4 border rounded-md shadow-sm space-y-1">
-      <p className="font-semibold">{item.address}</p>
-      <p className="text-sm text-gray-500">{item.memo}</p>
-      <p className="text-xs text-gray-400">
+      <div className="font-semibold">
+        장소 이름
+        <span className="font-normal text-gray600 ml-4">{item.address}</span>
+      </div>
+      <p className="text-sm text-gray600">여기는 메모 자리 - {item.memo}</p>
+      <p className="text-xs text-gray600">
         위도: {item.lat}, 경도: {item.lng}
       </p>
     </li>
