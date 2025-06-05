b5e5019fe298f1f737a4bc1e97628540441a672a
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-05 19:43:02 +0900

    feat: 프로필 정보 섹션 추가

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,7 +1,12 @@
+import SettingComponent from "@/components/Setting/SettingComponent";
 import React from "react";
 
 const SettingPage = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-3xl mx-auto">
+      <SettingComponent />
+    </div>
+  );
 };
 
 export default SettingPage;
