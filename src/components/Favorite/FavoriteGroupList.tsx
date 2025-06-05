9c495c53c943a50675f34758bca3697df8e6443f
SangHwi <brilyent72@gmail.com>
2025-06-05 16:49:36 +0900

    feat: 즐겨찾기 그룹 리스트 에러, 로딩 핸들링 에러 바운더리 적용

--- src/components/Favorite/FavoriteGroupList.tsx
+++ src/components/Favorite/FavoriteGroupList.tsx
@@ -6,12 +6,14 @@ import { useFavoriteGroups } from "@/hooks/useFavorite";
 import type { FavoriteGroupType } from "@/types/favorite";
 import { createFavoriteGroup } from "@/lib/api/favorite";
 import FavoriteGroupCard from "@/components/Favorite/FavoriteGroupCard";
+import LoadingSpinnerBoundary from "../common/LoadingSpinner/LoadingSpinnerBoundary";
+import AppErrorBoundary from "../common/ErrorBoundary/ErrorBoundary";
 
 export default function FavoriteGroupList() {
   const { user } = useAuthStore();
   const userId = user?.userId ?? null;
 
-  const { data: groups, isLoading, error } = useFavoriteGroups(userId);
+  const { data: groups, isLoading } = useFavoriteGroups(userId);
   const [groupList, setGroupList] = useState<FavoriteGroupType[]>([]);
 
   // 초기 값을 setGroupList를 통해 useState 로 저장
@@ -23,7 +25,7 @@ export default function FavoriteGroupList() {
     setGroupList((prev) => prev.filter((group) => group.groupId !== groupId));
   };
 
-  if (isLoading) return <div>로딩 중...</div>;
+  if (isLoading) return <LoadingSpinnerBoundary />;
 
   if (!groupList || groupList.length === 0)
     return <div>즐겨찾기 그룹이 없습니다.</div>;
@@ -43,25 +45,27 @@ export default function FavoriteGroupList() {
   };
 
   return (
-    <div className="space-y-2">
-      {/* 그룹 추가 버튼 */}
-      <div className="flex justify-end mb-2">
-        <button
-          onClick={handleCreateGroup}
-          className="text-sm text-violet600 underline hover:text-violet800"
-        >
-          그룹 추가
-        </button>
-      </div>
+    <AppErrorBoundary fallbackMessage="즐겨찾기 그룹을 불러오는 중 오류가 발생했습니다.">
+      <div className="space-y-2">
+        {/* 그룹 추가 버튼 */}
+        <div className="flex justify-end mb-2">
+          <button
+            onClick={handleCreateGroup}
+            className="text-sm text-violet600 underline hover:text-violet800"
+          >
+            그룹 추가
+          </button>
+        </div>
 
-      {/* 즐겨찾기 그룹 리스트 */}
-      <ul className="space-y-2">
-        {groupList.map((group) => (
-          <li key={group.groupId}>
-            <FavoriteGroupCard group={group} onDelete={handleDeleteGroup} />
-          </li>
-        ))}
-      </ul>
-    </div>
+        {/* 즐겨찾기 그룹 리스트 */}
+        <ul className="space-y-2">
+          {groupList.map((group) => (
+            <li key={group.groupId}>
+              <FavoriteGroupCard group={group} onDelete={handleDeleteGroup} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </AppErrorBoundary>
   );
 }
