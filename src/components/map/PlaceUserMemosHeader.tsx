84eae0935d098655a73cabe5cb32524c5b176207
saaad99 <ehdgnstla@naver.com>
2025-06-05 13:14:12 +0900

    ref : PlaceUserMemos 에서 헤더 분리하여 컴포넌트화

--- src/components/map/PlaceUserMemosHeader.tsx
+++ src/components/map/PlaceUserMemosHeader.tsx
@@ -18,29 +18,31 @@ export default function PlaceUserMemosHeader({
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <button className="absolute left-4 top-4" onClick={backPage}>
-        <ArrowLeftIcon />
-      </button>
-
-      <div className="flex items-center space-x-2">
-        <button onClick={handleClickFavorite} className="flex self-start">
-          <LucideStar
-            className={favorite ? " " : "text-yellow-400 fill-yellow-400"}
-          />
+    <div className="relative w-full px-4 py-4">
+      <div className="flex flex-col items-center justify-center">
+        <button className="absolute left-4 top-4" onClick={backPage}>
+          <ArrowLeftIcon />
         </button>
 
-        <div className="flex flex-col justify-center items-center text-center max-w-[200px]">
-          <span className="font-bold text-gray900 text-lg leading-snug break-words">
-            {placeDetail?.placeName}
-          </span>
-          <span className="text-xs text-gray600 leading-snug break-words">
-            {placeDetail?.address}
-          </span>
+        <div className="flex items-center space-x-2">
+          <button onClick={handleClickFavorite} className="flex self-start">
+            <LucideStar
+              className={favorite ? " " : "text-yellow-400 fill-yellow-400"}
+            />
+          </button>
+
+          <div className="flex flex-col justify-center items-center text-center max-w-[200px]">
+            <span className="font-bold text-gray900 text-lg leading-snug break-words">
+              {placeDetail?.placeName}
+            </span>
+            <span className="text-xs text-gray600 leading-snug break-words">
+              {placeDetail?.address}
+            </span>
+          </div>
+          <button className="absolute top-4 right-4 w-5 h-5 mt-0.5">
+            <MoreVerticalIcon />
+          </button>
         </div>
-        <button className="absolute top-4 right-4 w-5 h-5 mt-0.5">
-          <MoreVerticalIcon />
-        </button>
       </div>
     </div>
   );
