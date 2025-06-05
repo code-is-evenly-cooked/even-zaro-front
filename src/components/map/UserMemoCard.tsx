eca9a13d08d44b3de5da201af619fec905b76668
saaad99 <ehdgnstla@naver.com>
2025-06-05 13:14:32 +0900

    ref : 유저 메모리스트, 유저 메모 카드 컴포넌트로 분리

--- src/components/map/UserMemoCard.tsx
+++ src/components/map/UserMemoCard.tsx
@@ -2,28 +2,27 @@ import Image from "next/image";
 import { getProfileImageUrl } from "@/utils/image";
 
 interface UserMemoProps {
-  profileImage : string;
-  nickName : string;
-  memo : string;
+  profileImage: string;
+  nickName: string;
+  memo: string;
 }
 
 export default function UserMemoCard({
   profileImage,
   nickName,
-  memo
-} : UserMemoProps) {
+  memo,
+}: UserMemoProps) {
   return (
     <li className="flex items-center hover:bg-gray100 transition p-1">
       {/* 이미지 영역 */}
       <button className="flex items-center justify-center rounded-full w-11 h-11 border-2 border-gray200">
-        <Image src={getProfileImageUrl(profileImage)}
-               alt="프로필 이미지"
-               width="40"
-               height="40"
-               className="rounded-full border-1 border-gray200 flex-shrink-0"
+        <Image
+          src={getProfileImageUrl(profileImage)}
+          alt="프로필 이미지"
+          width="40"
+          height="40"
+          className="rounded-full border-1 border-gray200 flex-shrink-0"
         />
-
-        {/*<img src={profileImage} className="rounded-full w-10 h-10 border-1 border-gray200 flex-shrink-0"/>*/}
       </button>
       <div className="items-center flex p-3 shadow-sm space-x-3">
         <span className="font-bold text-base flex-shrink-0">{nickName}</span>
---
+++ src/components/map/UserMemoCards.tsx
@@ -0,0 +1,23 @@
+import UserMemoCard from "@/components/map/UserMemoCard";
+import React from "react";
+import { PlaceDetailResponse } from "@/types/map";
+
+interface UserMemoCardsProps {
+  placeDetail: PlaceDetailResponse;
+}
+
+export function UserMemoCards ( {placeDetail} : UserMemoCardsProps ) {
+
+  return(
+    <ul className="flex flex-col gap-3 px-4 py-4 overflow-y-auto">
+      {placeDetail?.usersInfo.map((user) => (
+        <UserMemoCard
+          key={user.userId}
+          profileImage={user.profileImage}
+          nickName={user.nickname}
+          memo={user.memo}
+        />
+      ))}
+    </ul>
+  );
+}
