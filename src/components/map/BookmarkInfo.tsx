9230cc5563b69f2e8972a55d4c9d478eed2fe015
saaad99 <ehdgnstla@naver.com>
2025-06-05 13:15:06 +0900

    ref : 즐겨찾기한 사용자 정보 BookmarkInfo 컴포넌트로 분리

--- src/components/map/BookmarkInfo.tsx
+++ src/components/map/BookmarkInfo.tsx
@@ -9,34 +9,42 @@ interface BookmarkInfoProps {
 
 export function BookmarkInfo({ placeDetail }: BookmarkInfoProps) {
   return (
-    <>
-      <ul className="flex -space-x-6">
-        {placeDetail?.usersInfo.slice(0, 3).map((user, idx) => (
-          <li
-            className="flex items-center justify-center rounded-full w-11 h-11 border-2 border-gray200"
-            key={idx}
-          >
-            <button>
-              <Image
-                src={getProfileImageUrl(user.profileImage)}
-                alt="유저 이미지"
-                className="rounded-full border-1 border-gray200 flex-shrink-0"
-                width="40"
-                height="40"
-              />
+    <div className="flex items-center space-x-2 justify-center pb-4">
+      {placeDetail?.favoriteCount == 0 ? (
+        <div className="text-xs">
+          <span> 아무도 해당 장소를 즐겨찾기에 등록하지 않았습니다.</span>
+        </div>
+      ) : (
+        <>
+          <ul className="flex -space-x-6">
+            {placeDetail?.usersInfo.slice(0, 3).map((user, idx) => (
+              <li
+                className="flex items-center justify-center rounded-full w-11 h-11 border-2 border-gray200"
+                key={idx}
+              >
+                <button>
+                  <Image
+                    src={getProfileImageUrl(user.profileImage)}
+                    alt="유저 이미지"
+                    className="rounded-full border-1 border-gray200 flex-shrink-0"
+                    width="40"
+                    height="40"
+                  />
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="text-xs">
+            <button className="font-bold">
+              {placeDetail?.usersInfo[0].nickname}
             </button>
-          </li>
-        ))}
-      </ul>
-      <div className="text-xs">
-        <button className="font-bold">
-          {placeDetail?.usersInfo[0].nickname}
-        </button>
-        <span>
-          {" "}
-          님 외 {placeDetail?.favoriteCount} 명이 즐겨찾기에 추가했습니다.
-        </span>
-      </div>
-    </>
+            <span>
+              {" "}
+              님 외 {placeDetail?.favoriteCount} 명이 즐겨찾기에 추가했습니다.
+            </span>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
