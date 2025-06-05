6a441be67256bea0a2bca9b28b530d19187edd3d
saaad99 <ehdgnstla@naver.com>
2025-06-05 18:02:42 +0900

    feat: PlaceUserMemos PropsDrilling 문제 수정

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -4,44 +4,20 @@ import KakaoMap from "@/components/map/KakaoMap";
 import SideMenu from "@/components/map/SideMenu";
 import PlaceModal from "@/components/map/PlaceModal";
 import PlaceUserMemos from "@/components/map/PlaceUserMemos";
-import { useEffect } from "react";
-import { fetchPlaceDetail } from "@/lib/api/map";
 import { PAGE } from "@/types/map";
 import { UserGroupList } from "@/components/map/UserGroupList";
 import { useMapStore } from "@/stores/mapStore";
 
 const MapPage = () => {
-  const { page, placeId, placeList, placeDetail, groupModal } = useMapStore((state) => state);
-  const { setPlaceDetail } = useMapStore();
-
-
-
-  useEffect(() => {
-    if (placeId !== null) {
-      (async () => {
-        try {
-          const data = await fetchPlaceDetail(placeId);
-          setPlaceDetail(data);
-        } catch (error) {
-          console.error("장소 상세 정보를 불러오는 데 실패했습니다.", error);
-        }
-      })();
-    }
-  }, [placeId]);
+  const { page, groupModal } = useMapStore((state) => state);
 
   return (
     <div>
       <KakaoMap />
       <SideMenu />
 
-      {page === PAGE.PLACELIST && placeList && (
-        <PlaceModal />
-      )}
-      {page == PAGE.PLACEDETAIL && placeDetail && (
-        <PlaceUserMemos
-          placeDetail={placeDetail}
-        />
-      )}
+      {page === PAGE.PLACELIST && <PlaceModal />}
+      {page === PAGE.PLACEDETAIL && <PlaceUserMemos />}
 
       {groupModal && <UserGroupList />}
     </div>
