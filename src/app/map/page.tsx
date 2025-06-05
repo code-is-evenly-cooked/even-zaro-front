c454dfb09cb7f6a5ada98bbb463b1886e0924bfb
saaad99 <ehdgnstla@naver.com>
2025-06-05 17:49:44 +0900

    feat: PlaceModal PropsDrilling 문제 수정

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -5,33 +5,16 @@ import SideMenu from "@/components/map/SideMenu";
 import PlaceModal from "@/components/map/PlaceModal";
 import PlaceUserMemos from "@/components/map/PlaceUserMemos";
 import { useEffect } from "react";
-import { fetchPlaceDetail, fetchPlaceList } from "@/lib/api/map";
-import { PAGE, PlaceListResponse } from "@/types/map";
+import { fetchPlaceDetail } from "@/lib/api/map";
+import { PAGE } from "@/types/map";
 import { UserGroupList } from "@/components/map/UserGroupList";
 import { useMapStore } from "@/stores/mapStore";
 
 const MapPage = () => {
   const { page, placeId, placeList, placeDetail, groupModal } = useMapStore((state) => state);
-  const { setPlaceList, setPlaceDetail } = useMapStore();
+  const { setPlaceDetail } = useMapStore();
 
-  useEffect(() => {
-    const lat = 37.554722;
-    const lng = 126.97083;
-    const distanceKm = 5;
 
-    (async () => {
-      try {
-        const data: PlaceListResponse = await fetchPlaceList(
-          lat,
-          lng,
-          distanceKm,
-        );
-        setPlaceList(data);
-      } catch (error) {
-        console.error("장소 목록을 불러오는 데 실패했습니다.", error);
-      }
-    })();
-  }, []);
 
   useEffect(() => {
     if (placeId !== null) {
@@ -52,7 +35,7 @@ const MapPage = () => {
       <SideMenu />
 
       {page === PAGE.PLACELIST && placeList && (
-        <PlaceModal placeList={placeList}></PlaceModal>
+        <PlaceModal />
       )}
       {page == PAGE.PLACEDETAIL && placeDetail && (
         <PlaceUserMemos
