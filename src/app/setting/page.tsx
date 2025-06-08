ee74f4a0d35f6b2e0b29b40d19c700464dbb7a55
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-08 22:14:56 +0900

    feat: 내 정보 받아오기 api 추가

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,10 +1,24 @@
 import SettingComponent from "@/components/Setting/SettingComponent";
-import React from "react";
+import { server } from "@/lib/fetch/server";
+import { UserInfo } from "@/stores/useAuthStore";
+import { APIErrorResponse } from "@/types/api";
 
-const SettingPage = () => {
+const SettingPage = async () => {
+  let user: UserInfo;
+
+  try {
+    user = await server<UserInfo>("/users/my", {
+      needAuth: true,
+    });
+  } catch (err) {
+    const statusCode = err instanceof APIErrorResponse ? err.statusCode : 500;
+    console.log(statusCode);
+    throw err; // error.tsx로 넘어감
+  }
+
   return (
     <div className="max-w-3xl mx-auto">
-      <SettingComponent />
+      <SettingComponent user={user} />
     </div>
   );
 };
