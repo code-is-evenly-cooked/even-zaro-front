6c296afc97c1957094af1d2f008b5a891f016fb6
saaad99 <ehdgnstla@naver.com>
2025-06-09 14:58:35 +0900

    style : 좌측 여백 제거

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -7,18 +7,23 @@ import PlaceUserMemos from "@/components/map/PlaceUserMemos";
 import { PAGE } from "@/types/map";
 import { UserGroupList } from "@/components/map/UserGroupList";
 import { useMapStore } from "@/stores/mapStore";
+import { FavoriteAddModal } from "@/components/map/FavoriteAddModal";
 
 const MapPage = () => {
   const { page } = useMapStore((state) => state);
 
   return (
-    <div>
+    <div className="w-full h-full">
       <KakaoMap />
       <SideMenu />
 
       {page === PAGE.PLACELIST && <PlaceModal />}
       {page === PAGE.PLACEDETAIL && <PlaceUserMemos />}
       {page === PAGE.USERGROUPLIST && <UserGroupList />}
+
+      <FavoriteAddModal />
+
+
     </div>
   );
 };
