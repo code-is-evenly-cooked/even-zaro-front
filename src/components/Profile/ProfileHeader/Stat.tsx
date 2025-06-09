c8e53efa1df6ce41c09b4e4eedb7286920af8feb
SangHwi <brilyent72@gmail.com>
2025-06-09 18:07:16 +0900

    style: 프로필 팔로잉 팔로워 글씨가 줄바꿈이 되지 않도록 스타일 지정

--- src/components/Profile/ProfileHeader/Stat.tsx
+++ src/components/Profile/ProfileHeader/Stat.tsx
@@ -1,6 +1,6 @@
 export function Stat({ label, count }: { label: string; count: number }) {
   return (
-    <li key={label} className="flex sm:flex-row flex-col-reverse gap-2 text-center">
+    <li key={label} className="flex sm:flex-row flex-col-reverse gap-2 text-center whitespace-nowrap">
       {label}
       <span className="font-bold text-center">{count}</span>
     </li>
