e884b045979727d2d5f6548a8e3f68aa410a2ce5
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-09 09:02:39 +0900

    feat: 정보용 공통 row ui 추가

---
+++ src/components/Setting/FormFieldRow.tsx
@@ -0,0 +1,24 @@
+interface FormFieldRowProps {
+  label: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const FormFieldRow = ({ label, children, className }: FormFieldRowProps) => {
+  return (
+    <li
+      className={`flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-6 ${className}`}
+      key={label}
+    >
+      <label
+        className="text-lg whitespace-nowrap shrink-0 w-24"
+        htmlFor={label}
+      >
+        {label}
+      </label>
+      {children}
+    </li>
+  );
+};
+
+export default FormFieldRow;
