de3d55cd553dfadf4c783eb39c713fb36d7df4cf
saaad99 <ehdgnstla@naver.com>
2025-06-09 17:43:37 +0900

    ref : 그룹 리스트 컴포넌트 리팩토링

---
+++ src/components/map/GroupCard.tsx
@@ -0,0 +1,35 @@
+import { MoreVertical, Star } from "lucide-react";
+import { GroupListResponse } from "@/types/map";
+
+
+interface GroupCardProps {
+  group : GroupListResponse,
+}
+
+export function GroupCard ( {group} : GroupCardProps) {
+  return (
+    <>
+      <li
+        className="flex items-center justify-between px-4 py-3 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-3">
+          <div className="w-8 h-8 rounded-full bg-violet-100 flex items-center justify-center">
+            <Star size={16} className="text-violet-500" />
+          </div>
+          <div>
+            <div className="font-medium text-sm text-gray-900">
+              {group.name}
+            </div>
+            <div className="text-xs text-gray-600">
+              {/* 여기 응답 객체 백엔드단 코드 추가 되면 수정해야함!!!!!!!!!!!1*/}
+              장소 {group.groupFavoriteCount}
+            </div>
+          </div>
+        </div>
+        <button className="hover:bg-gray-200 rounded-full p-1">
+          <MoreVertical size={20} className="text-gray-400" />
+        </button>
+      </li>
+    </>
+  );
+}
---
+++ src/components/map/GroupCardList.tsx
@@ -0,0 +1,35 @@
+import FallbackMessage from "@/components/common/Fallback/FallbackMessage";
+import { useEffect } from "react";
+import { GroupListResponseList } from "@/types/map";
+import { fetchGroupList } from "@/lib/api/map";
+import { useMapStore } from "@/stores/mapStore";
+import { GroupCard } from "@/components/map/GroupCard";
+
+export function GroupCardList() {
+  const { groupList, otherUserId } = useMapStore((state) => state);
+  const { setGroupList } = useMapStore();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (otherUserId != null) {
+          const data: GroupListResponseList = await fetchGroupList(otherUserId);
+          setGroupList(data);
+        }
+      } catch (error) {
+        console.error("유저의 그룹 리스트를 불러오는 데 실패했습니다.", error);
+      }
+    })();
+  }, [otherUserId]);
+  return (
+    <>
+      <ul className="flex-1 overflow-y-auto divide-y">
+        {groupList && groupList.length > 0 ? (
+          groupList.map((group, idx) => <GroupCard group={group} key={idx} />)
+        ) : (
+          <FallbackMessage className="등록된 그룹이 없습니다." />
+        )}
+      </ul>
+    </>
+  );
+}
