c1e7ef956a5f053dba585e2d81a3f1c77d86a092
saaad99 <ehdgnstla@naver.com>
2025-06-10 21:52:56 +0900

    ref: 조회된 장소를 마커 표시하는 함수 정의 (아직 동작x)

--- src/components/map/KakaoMap.tsx
+++ src/components/map/KakaoMap.tsx
@@ -5,14 +5,14 @@ import {
   addMarkers,
   initializeMap,
   loadKakaoMapSdk,
-  moveMyLocation,
+  moveMyLocation, placeToMarker,
   updateCenterAddress,
 } from "@/utils/mapUtil";
-import { useMapStore } from "@/stores/mapStore"; // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+import { useMapStore } from "@/stores/mapStore";
 
 export default function KakaoMap() {
   const mapRef = useRef<HTMLDivElement>(null);
-  const myLocation = useMapStore((state) => state.myLocation);
+  const { placeList } = useMapStore((state) => state);
   const { setMyLocation, setRegionName } = useMapStore();
 
   useEffect(() => {
@@ -22,13 +22,10 @@ export default function KakaoMap() {
         addMarkers(map); // 마커 추가 테스트
         moveMyLocation(map, setMyLocation); // 내 위치 추적하여 전역상태변수에 위도경도 저장
         updateCenterAddress(map, setRegionName); // 지도 중심 주소 업데이트 및 내 위치 행정동 저장
+        placeToMarker(placeList!, map);
       });
     });
   }, []);
-
-  useEffect(() => {
-    console.log("전역상태 myLocation :", myLocation);
-  }, [myLocation]);
 
   return (
     <>
--- src/utils/mapUtil.ts
+++ src/utils/mapUtil.ts
@@ -14,6 +14,8 @@
 //   }
 // }
 
+import { markerInfo, PlaceListResponse } from "@/types/map";
+
 declare global {
   interface Window {
     kakao: any; // kakao.maps.* 전부 포괄
@@ -172,4 +174,41 @@ export function updateCenterAddress(
       },
     );
   });
+}
+
+
+export function placeToMarker(places: PlaceListResponse, map: any) {
+  const positions: markerInfo[] = [];
+
+  places.placeInfos.map((place) => {
+    const position: markerInfo = {
+      title: place.name,
+      latlng: { lat: place.lat, lng: place.lng },
+    };
+    positions.push(position);
+  });
+
+  console.log("positions", positions);
+
+  // 마커 이미지의 이미지 주소입니다
+  const imageSrc =
+    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+  for (let i = 0; i < positions.length; i++) {
+    // 마커 이미지의 이미지 크기 입니다
+    const imageSize = new window.kakao.maps.Size(24, 35);
+
+    // 마커 이미지를 생성합니다
+    const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    // 마커를 생성합니다
+    const marker = new window.kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: positions[i].latlng, // 마커를 표시할 위치
+      title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      image: markerImage, // 마커 이미지
+    });
+
+    marker.setMap(map);
+  }
 }
