287d8462f729eff5b7c740294ff871088bcca93e
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-10 17:02:12 +0900

    feat: 신고하기 버튼 액션 추가

--- src/components/Report/ReportComponent.tsx
+++ src/components/Report/ReportComponent.tsx
@@ -3,6 +3,7 @@ import { ReportReason, ReportType } from "@/types/report";
 import BaseButton from "../common/Button/BaseButton";
 import ReportSelector from "./ReportSelector";
 import { useState } from "react";
+import { useToastMessageContext } from "@/providers/ToastMessageProvider";
 
 interface ReportComponentProps {
   reportId: string;
@@ -15,9 +16,37 @@ const ReportComponent = ({ reportId, type }: ReportComponentProps) => {
     null,
   );
   const [etcReason, setEtcReason] = useState("");
+
+  const { showToastMessage } = useToastMessageContext();
+
+  const handleSubmit = () => {
+    if (!selectedReason) {
+      showToastMessage({ type: "info", message: "신고 사유를 선택해주세요." });
+      return;
+    }
+
+    if (selectedReason === ReportReason.ETC && etcReason.trim() === "") {
+      showToastMessage({
+        type: "info",
+        message: "기타 사유에 대해 간단히 설명해주세요.",
+      });
+      return;
+    }
+
+    const payload = {
+      reason: selectedReason,
+      detail: selectedReason === ReportReason.ETC ? etcReason : null,
+    };
+
+    console.log(payload);
+  };
+
   return (
     <div className="flex flex-col gap-8">
-      <h1 className="text-2xl font-bold">신고 하기</h1>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">신고 하기</h1>
+        <h2>신고 사유를 선택해주세요.</h2>
+      </div>
       <ReportSelector
         selected={selectedReason}
         etcReason={etcReason}
@@ -28,6 +57,7 @@ const ReportComponent = ({ reportId, type }: ReportComponentProps) => {
         color="violet800"
         size="xl"
         className="items-center mx-auto w-6/12 min-w-20"
+        onClick={handleSubmit}
       >
         신고하기
       </BaseButton>
