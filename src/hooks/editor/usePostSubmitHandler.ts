960ae24c78ed93edbc46c4899db7896cb0c10a14
SangHwi <brilyent72@gmail.com>
2025-06-11 14:26:24 +0900

    feat: 게시글 수정 훅 적용

--- src/hooks/editor/usePostSubmitHandler.ts
+++ src/hooks/editor/usePostSubmitHandler.ts
@@ -1,12 +1,14 @@
-import { useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useToastMessageContext } from "@/providers/ToastMessageProvider";
-import { createPost } from "@/lib/api/post.client";
+import { createPost, updatePost } from "@/lib/api/post.client";
 import { extractImageKeys, extractThumbnailKey } from "@/utils/editorImage";
 import { usePostStore } from "@/stores/usePostStore";
 import { MainCategory } from "@/types/category";
 
 export function usePostSubmitHandler() {
+  const searchParams = useSearchParams();
   const router = useRouter();
+  const postId = searchParams.get("postId");
   const { showToastMessage } = useToastMessageContext();
   const {
     title,
@@ -42,21 +44,42 @@ export function usePostSubmitHandler() {
       setImageList(imageUrls);
       setThumbnailImage(thumbnail);
 
-      const { category, postId } = await createPost({
-        title,
-        content,
-        category: mainCategory as MainCategory,
-        tag: subCategory,
-        postImageList: imageUrls,
-        thumbnailImage: thumbnail,
-      });
+      if (postId) {
+        // 수정 모드
+        await updatePost(Number(postId), {
+          title,
+          content,
+          tag: subCategory,
+          postImageList: imageUrls ?? [],
+          thumbnailImage: thumbnail ?? null,
+        });
 
-      showToastMessage({
-        message: "게시글 작성이 완료되었습니다!",
-        type: "success",
-      });
-      resetPost();
-      router.replace(`/board/${category}/${postId}`);
+        showToastMessage({
+          message: "게시글 수정이 완료되었습니다!",
+          type: "success",
+        });
+
+        resetPost();
+        router.replace(`/board/${mainCategory}/${postId}`);
+      } else {
+        // 작성 모드
+        const { category, postId: newPostId } = await createPost({
+          title,
+          content,
+          category: mainCategory as MainCategory,
+          tag: subCategory,
+          postImageList: imageUrls,
+          thumbnailImage: thumbnail,
+        });
+
+        showToastMessage({
+          message: "게시글 작성이 완료되었습니다!",
+          type: "success",
+        });
+
+        resetPost();
+        router.replace(`/board/${category}/${newPostId}`);
+      }
     } catch (error) {
       const errorMessage =
         error instanceof Error
