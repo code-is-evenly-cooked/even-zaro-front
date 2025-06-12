7393e13ca4743bc23e3e26a69e8e631d17b3ec7f
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-12 14:58:01 +0900

    feat: 헤더 Stat내부에 버튼 액션 추가

--- src/components/Profile/ProfileHeader/Stat.tsx
+++ src/components/Profile/ProfileHeader/Stat.tsx
@@ -1,6 +1,24 @@
-export function Stat({ label, count }: { label: string; count: number }) {
+import clsx from "clsx";
+
+interface StatProps {
+  label: string;
+  count: number;
+  onClick?: () => void;
+  className?: string;
+}
+
+export function Stat({ label, count, onClick, className }: StatProps) {
   return (
-    <li key={label} className="flex sm:flex-row flex-col-reverse gap-2 text-center whitespace-nowrap">
+    <li
+      role="button"
+      key={label}
+      onClick={onClick}
+      className={clsx(
+        "flex sm:flex-row flex-col-reverse gap-2 text-center whitespace-nowrap",
+        onClick && "cursor-pointer",
+        className,
+      )}
+    >
       {label}
       <span className="font-bold text-center">{count}</span>
     </li>
