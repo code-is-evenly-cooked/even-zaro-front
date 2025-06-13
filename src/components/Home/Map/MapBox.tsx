3aef4e1087ce18d42f23c155f958c45815fadb25
SangHwi <brilyent72@gmail.com>
2025-06-13 12:19:20 +0900

    feat: 홈 화면 현재 위치 중심 지도 구현

---
+++ src/components/Home/Map/MapBox.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { loadKakaoMapSdk } from "@/utils/mapUtil";
+
+export default function MapBox() {
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  // 현재 위치 중심 지도
+  useEffect(() => {
+    loadKakaoMapSdk(() => {  // 카카오 sdk 불러오기
+      if (!window.kakao || !window.kakao.maps) return;
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const container = mapRef.current;
+        if (!container) return;
+
+        const map = new window.kakao.maps.Map(container, {
+          center: new window.kakao.maps.LatLng(latitude, longitude),
+          level: 3,
+        });
+
+        new window.kakao.maps.Marker({
+          map,
+          position: new window.kakao.maps.LatLng(latitude, longitude),
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div
+      ref={mapRef}
+      className="w-full md:w-[600px] h-[360px] bg-gray200 rounded-2xl"
+    />
+  );
+}
