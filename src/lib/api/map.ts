5bc306f56d2d30ac117080d6a708b087e564b985
saaad99 <ehdgnstla@naver.com>
2025-06-13 17:26:34 +0900

    feat: 즐겨찾기 추가 api

--- src/lib/api/map.ts
+++ src/lib/api/map.ts
@@ -1,16 +1,18 @@
 import { client } from "@/lib/fetch/client";
 import {
-  PlaceListResponse,
-  PlaceDetailResponse,
-  GroupListResponse,
+  FavoriteAddRequest,
   FavoriteListResponse,
+  FavroiteAddResponse,
+  GroupListResponse,
+  PlaceDetailResponse,
+  PlaceListResponse,
 } from "@/types/map";
 
 // lat, lng, distanceKm을 파라미터로 받아 사용
 export const fetchPlaceList = async (
   lat: number,
   lng: number,
-  distanceKm: number
+  distanceKm: number,
 ): Promise<PlaceListResponse> => {
   const url = `/map/place?lat=${lat}&lng=${lng}&distanceKm=${distanceKm}`;
   return await client<PlaceListResponse>(url, {
@@ -27,20 +29,43 @@ export const fetchPlaceDetail = async (placeId: number): Promise<PlaceDetailResp
 export const fetchGroupList = async (userId: number): Promise<GroupListResponse[]> => {
   return await client<GroupListResponse[]>(`/group/user/${userId}/group`, {
     method: "GET",
-  })
-}
+  });
+};
 
-export const fetchFavoriteStatus = async(placeId : number) : Promise<boolean> => {
+export const fetchFavoriteStatus = async (placeId: number): Promise<boolean> => {
   return await client<boolean>(`/favorite/check`, {
     method: "GET",
     params: {
       placeId: placeId,
-    }
-  })
-}
+    },
+  });
+};
 
 export const fetchFavoritesByGroup = async (groupId: number): Promise<FavoriteListResponse[]> => {
   return await client<FavoriteListResponse[]>(`/favorite/${groupId}/items`, {
     method: "GET",
-  })
-}
+  });
+};
+
+export const postAddFavorite = async (groupId: number, {
+  kakaoPlaceId,
+  memo,
+  placeName,
+  address,
+  lat,
+  lng,
+  category,
+}: FavoriteAddRequest): Promise<FavroiteAddResponse> => {
+  return await client<FavroiteAddResponse>(`/api/favorite/groups/${groupId}/favorites`, {
+    method: "POST",
+    body: JSON.stringify({
+      kakaoPlaceId : kakaoPlaceId,
+      memo: memo,
+      placeName: placeName,
+      address: address,
+      lat: lat,
+      lng: lng,
+      category: category,
+    }),
+  });
+};
