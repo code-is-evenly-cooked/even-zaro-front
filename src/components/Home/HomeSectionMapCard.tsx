b145b19a05b2fb2d2759012a0f796c94ac53097b
SangHwi <brilyent72@gmail.com>
2025-06-16 07:39:00 +0900

    feat: 핫플레이스 지도 이동 버튼 추가

--- src/components/Home/HomeSectionMapCard.tsx
+++ src/components/Home/HomeSectionMapCard.tsx
@@ -1,6 +1,6 @@
 import MapBox from "./Map/MapBox";
 import HotPlaceList from "./Map/HotPlaceList";
-// import ExploreButton from "./Map/ExploreButton";
+import ExploreButton from "./Map/ExploreButton";
 
 export default function HomeSectionMapCard() {
   return (
@@ -12,7 +12,7 @@ export default function HomeSectionMapCard() {
         <MapBox />
         <div className="flex flex-col">
           <HotPlaceList />
-          {/* <ExploreButton /> */}
+          <ExploreButton />
         </div>
       </div>
     </div>
---
+++ src/components/Home/Map/ExploreButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
+export default function ExploreButton() {
+  return (
+    <Link
+      href="/map"
+      className="w-full h-[72px] m-2 flex items-center justify-center rounded-2xl bg-violet800 text-white text-xl font-bold shadow-md hover:bg-violet700 transition-colors"
+    >
+      🏡 동네 탐방
+    </Link>
+  );
+}
