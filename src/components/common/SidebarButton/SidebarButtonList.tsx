a00f4fd83aa149b1a4578464fa6ae2cd2e8166b5
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-16 18:05:32 +0900

    feat: 사이드바 동네 탐방 아이콘 추가

--- src/components/common/SidebarButton/SidebarButtonList.tsx
+++ src/components/common/SidebarButton/SidebarButtonList.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { HomeIcon, ShoppingBagIcon, TipIcon, TogetherIcon } from "../Icons";
+import {
+  HomeIcon,
+  LocationIcon,
+  ShoppingBagIcon,
+  TipIcon,
+  TogetherIcon,
+} from "../Icons";
 import SidebarButton from "./SidebarButton";
 
 const buttons = [
@@ -7,6 +13,7 @@ const buttons = [
   { title: "같이 쓰자", icon: <TogetherIcon />, href: "/board/TOGETHER" },
   { title: "자취 일상", icon: <TipIcon />, href: "/board/DAILY_LIFE" },
   { title: "텅장 일기", icon: <ShoppingBagIcon />, href: "/board/RANDOM_BUY" },
+  { title: "동네 탐방", icon: <LocationIcon />, href: "/map" },
 ];
 
 interface SidebarButtonListProps {
