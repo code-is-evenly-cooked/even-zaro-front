63f7dbc10e5279b0e3274d2b91428f0073ab67e9
saaad99 <ehdgnstla@naver.com>
2025-06-17 15:52:20 +0900

    ref : useMapPlaceStore.ts 추가 - 장소 관련 데이터 관리용

---
+++ src/stores/useMapPlaceStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import {
+  KakaoMapResponse,
+  PlaceDetailResponse,
+  PlaceListResponse,
+} from "@/types/map";
+
+interface useMapPlaceStore {
+  // 장소 리스트
+  placeList: PlaceListResponse | null;
+  setPlaceList: (placeList: PlaceListResponse | null) => void;
+
+  // 장소 상세
+  placeDetail: PlaceDetailResponse | null;
+  setPlaceDetail: (placeDetail: PlaceDetailResponse) => void;
+
+  // 선택한 장소의 상세 정보
+  selectPlaceDetail: KakaoMapResponse | null;
+  setSelectPlaceDetail: (selectPlaceInfo: KakaoMapResponse | null) => void;
+}
+
+export const useMapPlaceStore = create<useMapPlaceStore>((set) => ({
+  // 초기값
+  placeList: null,
+  placeDetail: null,
+  selectPlaceDetail: null,
+
+
+  setPlaceList: (placeList: PlaceListResponse | null) =>
+    set(() => ({
+      placeList: placeList,
+    })),
+  setPlaceDetail: (placeDetail: PlaceDetailResponse) =>
+    set(() => ({
+      placeDetail: placeDetail,
+    })),
+  setSelectPlaceDetail: (selectPlaceDetail: KakaoMapResponse | null) =>
+    set(() => ({
+      selectPlaceDetail: selectPlaceDetail,
+    })),
+}));
