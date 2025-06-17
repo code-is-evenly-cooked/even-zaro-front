2724c1a4f01a8dcf9156c5bf58046f391e3409bd
saaad99 <ehdgnstla@naver.com>
2025-06-17 16:12:38 +0900

    ref : useMapGroupStore.ts 분리

--- src/stores/map/mapStore.ts
+++ src/stores/map/mapStore.ts
@@ -1,21 +1,12 @@
 import { create } from "zustand";
-import {
-  GroupListResponse, MyLoc,
-} from "@/types/map";
+import { MyLoc } from "@/types/map";
 
 interface MapStore {
-  // 유저 그룹 리스트
-  groupList: GroupListResponse[] | null;
-  setGroupList: (groupList: GroupListResponse[]) => void;
-
-  groupInfo: GroupListResponse | null;
-  setGroupInfo: (groupInfo : GroupListResponse) => void;
-
   // 내 위치
-  myLocation : MyLoc | null;
+  myLocation: MyLoc | null;
   regionName: string | null;
-  setMyLocation: (myLocation : {lat: number, lng: number}) => void;
-  setRegionName: (myRegionName : string) => void;
+  setMyLocation: (myLocation: { lat: number; lng: number }) => void;
+  setRegionName: (myRegionName: string) => void;
 
   // 지도 객체
   map: kakao.maps.Map | null;
@@ -24,25 +15,15 @@ interface MapStore {
 
 export const useMapStore = create<MapStore>((set) => ({
   // 초기값
-  groupList: [],
-  groupInfo: null,
-  myLocation : null,
+  myLocation: null,
   regionName: null,
   map: null,
 
-  setGroupList: (groupList: GroupListResponse[]) =>
-    set(() => ({
-      groupList: groupList,
-    })),
-  setGroupInfo: (groupInfo: GroupListResponse) =>
-    set(() => ({
-      groupInfo: groupInfo,
-    })),
-  setMyLocation: (myLocation : MyLoc) =>
+  setMyLocation: (myLocation: MyLoc) =>
     set(() => ({
       myLocation: myLocation,
     })),
-  setRegionName: (regionName : string) =>
+  setRegionName: (regionName: string) =>
     set(() => ({
       regionName: regionName,
     })),
---
+++ src/stores/map/useMapGroupStore.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { GroupListResponse } from "@/types/map";
+
+interface useMapGroupStore {
+  // 유저 그룹 리스트
+  groupList: GroupListResponse[] | null;
+  setGroupList: (groupList: GroupListResponse[]) => void;
+
+  groupInfo: GroupListResponse | null;
+  setGroupInfo: (groupInfo: GroupListResponse) => void;
+}
+
+export const useMapGroupStore = create<useMapGroupStore>((set) => ({
+  // 초기값
+  groupList: [],
+  groupInfo: null,
+
+  setGroupList: (groupList: GroupListResponse[]) =>
+    set(() => ({
+      groupList: groupList,
+    })),
+  setGroupInfo: (groupInfo: GroupListResponse) =>
+    set(() => ({
+      groupInfo: groupInfo,
+    })),
+}));
