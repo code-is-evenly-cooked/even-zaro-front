699922a364a8b95986d44f1f77dfaf6d9a13a5de
saaad99 <ehdgnstla@naver.com>
2025-06-17 12:05:03 +0900

    ref : 버튼 태그 삭제

--- src/components/map/UserMemoCard.tsx
+++ src/components/map/UserMemoCard.tsx
@@ -21,17 +21,18 @@ export default function UserMemoCard({
   return (
     <li className="flex items-center hover:bg-gray100 transition p-1">
       {/* 이미지 영역 */}
-      <button className="flex items-center justify-center rounded-full w-11 h-11 border-2 border-gray200">
-        <Link href={`/profile/${userId}`} className="flex items-center gap-2">
-          <Image
-            src={getProfileImageUrl(profileImage)}
-            alt="프로필 이미지"
-            width={40}
-            height={40}
-            className="rounded-full border border-gray200 flex-shrink-0 h-auto"
-          />
-        </Link>
-      </button>
+      <Link
+        href={`/profile/${userId}`}
+        className="flex items-center justify-center rounded-full w-11 h-11 border-2 border-gray200"
+      >
+        <Image
+          src={getProfileImageUrl(profileImage)}
+          alt="프로필 이미지"
+          width={40}
+          height={40}
+          className="rounded-full border border-gray200 flex-shrink-0 h-auto"
+        />
+      </Link>
       <div className="items-center flex p-3 shadow-sm space-x-3">
         <button
           onClick={() => setPageGroupList(userId)}
