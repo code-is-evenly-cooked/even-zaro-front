21853a7db63be790364aa9f27e83fef6d57fdca2
seongmin <min@Seongminui-MacBookPro-2.local>
2025-06-19 14:08:26 +0900

    feat: 유저 근처 정보 받아올 때 access token 보내지 않도록 수정

--- src/lib/api/map.ts
+++ src/lib/api/map.ts
@@ -17,22 +17,29 @@ export const fetchPlaceList = async (
   const url = `/map/place?lat=${lat}&lng=${lng}&distanceKm=${distanceKm}`;
   return await client<PlaceListResponse>(url, {
     method: "GET",
+    needAuth: false,
   });
 };
 
-export const fetchPlaceDetail = async (placeId: number): Promise<PlaceDetailResponse> => {
+export const fetchPlaceDetail = async (
+  placeId: number,
+): Promise<PlaceDetailResponse> => {
   return await client<PlaceDetailResponse>(`/map/place/${placeId}`, {
     method: "GET",
   });
 };
 
-export const fetchGroupList = async (userId: number): Promise<GroupListResponse[]> => {
+export const fetchGroupList = async (
+  userId: number,
+): Promise<GroupListResponse[]> => {
   return await client<GroupListResponse[]>(`/group/user/${userId}/group`, {
     method: "GET",
   });
 };
 
-export const fetchFavoriteStatus = async (placeId: number): Promise<boolean> => {
+export const fetchFavoriteStatus = async (
+  placeId: number,
+): Promise<boolean> => {
   return await client<boolean>(`/favorite/check`, {
     method: "GET",
     params: {
@@ -41,7 +48,9 @@ export const fetchFavoriteStatus = async (placeId: number): Promise<boolean> => 
   });
 };
 
-export const fetchFavoritesByGroup = async (groupId: number): Promise<FavoriteListResponse[]> => {
+export const fetchFavoritesByGroup = async (
+  groupId: number,
+): Promise<FavoriteListResponse[]> => {
   return await client<FavoriteListResponse[]>(`/favorite/${groupId}/items`, {
     method: "GET",
   });
@@ -52,30 +61,36 @@ export const postAddGroup = async (groupName: string): Promise<string> => {
   return await client<string>(`/group`, {
     method: "POST",
     body: JSON.stringify({
-      groupName: groupName
-    })
+      groupName: groupName,
+    }),
   });
 };
 
-export const postAddFavorite = async (groupId: number, {
-  kakaoPlaceId,
-  memo,
-  placeName,
-  address,
-  lat,
-  lng,
-  category,
-}: FavoriteAddRequest): Promise<FavroiteAddResponse> => {
-  return await client<FavroiteAddResponse>(`/favorite/groups/${groupId}/favorites`, {
-    method: "POST",
-    body: JSON.stringify({
-      kakaoPlaceId : kakaoPlaceId,
-      memo: memo,
-      placeName: placeName,
-      address: address,
-      lat: lat,
-      lng: lng,
-      category: category,
-    }),
-  });
+export const postAddFavorite = async (
+  groupId: number,
+  {
+    kakaoPlaceId,
+    memo,
+    placeName,
+    address,
+    lat,
+    lng,
+    category,
+  }: FavoriteAddRequest,
+): Promise<FavroiteAddResponse> => {
+  return await client<FavroiteAddResponse>(
+    `/favorite/groups/${groupId}/favorites`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        kakaoPlaceId: kakaoPlaceId,
+        memo: memo,
+        placeName: placeName,
+        address: address,
+        lat: lat,
+        lng: lng,
+        category: category,
+      }),
+    },
+  );
 };
